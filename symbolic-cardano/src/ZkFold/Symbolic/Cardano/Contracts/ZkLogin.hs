aa4a470e6b9b215ba2d0d27927a4eb2a3c6eef36
Vladimir <me.morozovvp@gmail.com>
2025-02-25 21:27:16 +1000

    No input validation

--- symbolic-cardano/src/ZkFold/Symbolic/Cardano/Contracts/ZkLogin.hs
+++ symbolic-cardano/src/ZkFold/Symbolic/Cardano/Contracts/ZkLogin.hs
@@ -1,17 +1,26 @@
+{-# LANGUAGE DeriveAnyClass       #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module ZkFold.Symbolic.Cardano.Contracts.ZkLogin
     ( PublicInput
+    , ZkLoginInput
     , zkLogin
     , zkLoginNoSig
     , zkLoginMock
     ) where
 
+import           GHC.Generics                         (Generic)
 import           Prelude                              (($))
 
 import           ZkFold.Symbolic.Algorithms.Hash.SHA2
 import           ZkFold.Symbolic.Algorithms.RSA
+import           ZkFold.Symbolic.Class
 import           ZkFold.Symbolic.Data.Bool
 import           ZkFold.Symbolic.Data.ByteString
+import           ZkFold.Symbolic.Data.Class
+import           ZkFold.Symbolic.Data.Combinators
 import           ZkFold.Symbolic.Data.Eq
+import           ZkFold.Symbolic.Data.Input
 import           ZkFold.Symbolic.Data.JWT             (Certificate, ClientSecret (..), SecretBits, TokenPayload (..),
                                                        secretBits, verifySignature)
 import           ZkFold.Symbolic.Data.VarByteString
@@ -51,14 +60,32 @@ zkLoginNoSig clientSecret@ClientSecret{..} amount recipient _ pi = piValid
         truePi = sha2Var @"SHA256" $ plEmail csPayload @+ fromByteString tokenHash @+ fromByteString amount @+ fromByteString recipient
         piValid = truePi == pi
 
+data ZkLoginInput ctx =
+    ZkLoginInput
+        { zkSecret      :: ClientSecret ctx
+        , zkAmount      :: ByteString 64 ctx
+        , zkRecipient   :: ByteString 256 ctx
+        , zkCertificate :: Certificate ctx
+        , zkPI          :: PublicInput ctx
+        }
+        deriving Generic
+
+deriving instance
+    ( Symbolic ctx
+    , KnownRegisters ctx PubExponentSize 'Auto
+    , KnownRegisters ctx 2048 'Auto
+    ) => SymbolicData (ZkLoginInput ctx)
+instance
+    ( Symbolic ctx
+    , KnownRegisters ctx PubExponentSize 'Auto
+    , KnownRegisters ctx 2048 'Auto
+    ) => SymbolicInput (ZkLoginInput ctx) where
+    isValid ZkLoginInput{..} = isValid zkAmount
+
 zkLoginMock
     :: forall ctx
     .  RSA 2048 10328 ctx
     => SecretBits ctx
-    => ClientSecret ctx
-    -> ByteString 64 ctx
-    -> ByteString 256 ctx
-    -> Certificate ctx
-    -> PublicInput ctx
+    => ZkLoginInput ctx
     -> Bool ctx
-zkLoginMock _ _ _ _ _ = true
+zkLoginMock _ = true
