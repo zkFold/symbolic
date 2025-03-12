61ccf0ff685a80ac543ab55914906956a59555fb
Eitan Chatav <eitan.chatav@gmail.com>
2025-03-12 08:51:14 -0700

    pluto eris module

---
+++ symbolic-base/src/ZkFold/Symbolic/Data/EllipticCurve/PlutoEris.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE RebindableSyntax     #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module ZkFold.Symbolic.Data.EllipticCurve.PlutoEris
+  (
+  ) where
