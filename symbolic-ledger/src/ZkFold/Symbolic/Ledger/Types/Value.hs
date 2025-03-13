4e4cce95612ebe1c8ff2ac8f4c9f296b93e58f91
sourabhxyz <lambdaproofs@pm.me>
2025-03-13 21:36:55 +0530

    feat(#509): try list folding against token names

--- symbolic-ledger/src/ZkFold/Symbolic/Ledger/Types/Value.hs
+++ symbolic-ledger/src/ZkFold/Symbolic/Ledger/Types/Value.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE BlockArguments #-}
 
 module ZkFold.Symbolic.Ledger.Types.Value (
   Token,
@@ -16,19 +17,23 @@ module ZkFold.Symbolic.Ledger.Types.Value (
 
 import           Data.Coerce                           (coerce)
 import           Data.Data                             (Proxy)
-import           Prelude                               hiding (Bool, Eq, all, length, null, splitAt, (&&), (*), (+),
+import           Prelude                               hiding ((||), Bool, Eq, all, length, null, splitAt, (&&), (*), (+),
                                                         (==))
 
 import           ZkFold.Base.Algebra.Basic.Class
 import           ZkFold.Symbolic.Class                 (Symbolic)
-import           ZkFold.Symbolic.Data.Bool             (Bool)
+import           ZkFold.Symbolic.Data.Bool             (Bool, BoolType (..))
 import           ZkFold.Symbolic.Data.Class            (SymbolicData (..), SymbolicOutput)
 import           ZkFold.Symbolic.Data.Combinators      (RegisterSize (Auto))
 import           ZkFold.Symbolic.Data.Conditional      (Conditional, ifThenElse)
+import qualified ZkFold.Symbolic.Data.List as Symbolic.List
 import           ZkFold.Symbolic.Data.Eq               (Eq (BooleanOf, (==)), SymbolicEq)
 import           ZkFold.Symbolic.Data.List             (List, emptyList, null, singleton, uncons, (.:))
 import           ZkFold.Symbolic.Data.UInt             (UInt)
 import           ZkFold.Symbolic.Ledger.Types.Contract (Contract, ContractId)
+import ZkFold.Symbolic.Data.Switch (Switch (..))
+import           ZkFold.Symbolic.Data.Morph        (MorphFrom, MorphTo (..), (@))
+import ZkFold.Symbolic.Fold (SymbolicFold)
 
 -- | Input to the minting contract. Usually a token name.
 data Token context
@@ -73,6 +78,36 @@ emptyMultiAssetValue ::
     => MultiAssetValue context
 emptyMultiAssetValue = UnsafeMultiAssetValue emptyList
 
+-- FIXME: delete me
+y ::
+     forall context.
+     SymbolicOutput (Token context)
+  => Eq (Token context)
+  => Context (Token context) ~ context
+  => SymbolicFold context
+  => List context (Token context)
+y = addToken (Morph \(a :: (Token s), b) -> a == b) undefined emptyList
+
+
+-- FIXME: delete me.
+addToken ::
+     forall context.
+     SymbolicOutput (Token context)
+  => Context (Token context) ~ context
+  => SymbolicFold context
+  => MorphFrom context (Token context, Token context) (Bool context)
+  -> Token context
+  -> List context (Token context) -> List context (Token context)
+addToken eq givenToken ls =
+  let (exists, _, r) =
+        Symbolic.List.foldr (Morph \(y :: Switch s (Token context), (found :: Bool s, givenToken' :: Switch s (Token context), ys)) ->
+          let isSame = eq @ (y, givenToken') in
+          (found || isSame, givenToken', y .: ys)
+              )
+              (false :: Bool context, givenToken, emptyList)
+              ls
+  in r
+
 -- | Add a single value to a multi-asset value.
 addValue ::
      forall context. Conditional (Bool context) (MultiAssetValue context)
@@ -93,13 +128,14 @@ addValue val@Value {..} (UnsafeMultiAssetValue valList) =
   let (valHead@(valHeadCurrencySymbol, valHeadTokenList), valTail) = uncons valList
       valHeadTokenListAdded = addTokenAmount valHeadTokenList
       UnsafeMultiAssetValue valTailAdded = addValue val (UnsafeMultiAssetValue valTail)
-      multiVal =
+  in ifThenElse (null valList)
+       (UnsafeMultiAssetValue (singleton (mintingPolicy, singleton (tokenInstance, tokenQuantity))))
+       (
         ifThenElse (mintingPolicy == valHeadCurrencySymbol)
           (UnsafeMultiAssetValue ((valHeadCurrencySymbol, valHeadTokenListAdded) .: valTail))
           (UnsafeMultiAssetValue (valHead .: valTailAdded))
-  in ifThenElse (null valList)
-       (UnsafeMultiAssetValue (singleton (mintingPolicy, singleton (tokenInstance, tokenQuantity))))
-       multiVal
+
+       )
   where
     addTokenAmount tokenAmountList =
       let (tokenHead@(tokenHeadToken, tokenHeadAmount), tokenTail) = uncons tokenAmountList
