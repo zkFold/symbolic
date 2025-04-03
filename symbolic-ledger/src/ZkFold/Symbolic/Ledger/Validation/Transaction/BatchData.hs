f7a5dc7ba43bffc3aa5398640ecb4212ed6e037b
sourabhxyz <lambdaproofs@pm.me>
2025-04-03 21:47:42 +0530

    feat(#539): check if list of online addresses is computed correctly in batch data validation

--- symbolic-ledger/src/ZkFold/Symbolic/Ledger/Validation/Transaction/BatchData.hs
+++ symbolic-ledger/src/ZkFold/Symbolic/Ledger/Validation/Transaction/BatchData.hs
@@ -2,13 +2,14 @@
 {-# LANGUAGE ImpredicativeTypes #-}
 module ZkFold.Symbolic.Ledger.Validation.Transaction.BatchData where
 import           Data.Function                    ((&))
+import           Prelude                          (fst, undefined, ($))
 
 import           ZkFold.Symbolic.Data.Bool
 import           ZkFold.Symbolic.Data.Conditional (ifThenElse)
 import           ZkFold.Symbolic.Data.Eq          ((==))
 import           ZkFold.Symbolic.Data.Hash        (preimage)
 import qualified ZkFold.Symbolic.Data.List        as Symbolic.List
-import           ZkFold.Symbolic.Data.List        (List)
+import           ZkFold.Symbolic.Data.List        (List, emptyList, (++), (.:))
 import           ZkFold.Symbolic.Data.Maybe
 import           ZkFold.Symbolic.Data.Morph
 import           ZkFold.Symbolic.Ledger.Types
@@ -26,19 +27,59 @@ validateTransactionBatchData' TransactionBatchData {..} TransactionBatchDataWitn
   -- * Offline txs list is computed correctly.
   -- * Txs are valid.
   -- TODO: Add more.
-  let (_resTxAccIx :: Maybe context (DAIndex context), resTxAccIsConsistent :: Bool context) =
-        Symbolic.List.foldr (  -- TODO: Document why we use foldr here.
+  let (_resTxAccIx :: Maybe context (DAIndex context), resTxAccIsConsistent :: Bool context
+       , resTxAccAddresses :: List context (Address context)) =
+        Symbolic.List.foldl (
         -- TODO: Remove type annotations as much as possible.
-          Morph \(tx :: Transaction s, (txAccIx :: Maybe s (DAIndex s), txAccIsConsistent :: Bool s)) ->
-            let (resInputsAccIx, resInputsAccIsConsistent, _) = 
+          Morph \((txAccIx :: Maybe s (DAIndex s), txAccIsConsistent :: Bool s, txAccAddresses :: List s (Address s)), tx :: Transaction s) ->
+            let (resInputsAccIx, resInputsAccIsConsistent, resInputsAccAddresses, _) =
                   Symbolic.List.foldl (
-                    Morph \((inputsAccIx :: Maybe s' (DAIndex s'), inputsAccIsConsistent :: Bool s', ownerAddr :: Address s'), input :: Input s') ->
+                    Morph \((inputsAccIx :: Maybe s' (DAIndex s'), inputsAccIsConsistent :: Bool s', inputsAccAddresses :: List s' (Address s'), ownerAddr :: Address s'), input :: Input s') ->
                       let inputAddr = txoAddress (txiOutput input)
-                          inputAddrIx = preimage inputAddr & (\(_, b, _) -> b) & just
-                          newIx = ifThenElse (isNothing inputsAccIx) inputAddrIx inputsAccIx
-                          newIsConsistent = ifThenElse (inputAddr == ownerAddr) (inputsAccIsConsistent && newIx == inputAddrIx) inputsAccIsConsistent
-                      in (newIx, newIsConsistent, ownerAddr)
-                  ) (txAccIx, txAccIsConsistent, txOwner tx) (txInputs tx) 
-                in (resInputsAccIx, resInputsAccIsConsistent)
-        ) (nothing :: Maybe context (DAIndex context), true :: Bool context) tbdwTransactions
-  in resTxAccIsConsistent
+                          (_inputAddrCir, inputAddrIx, inputAddrType) = preimage inputAddr
+                          minputAddrIx = just inputAddrIx
+                          newIx = ifThenElse (isNothing inputsAccIx) minputAddrIx inputsAccIx
+                          (newIsConsistent, newAddresses) =
+                            ifThenElse (inputAddr == ownerAddr)
+                              ( inputsAccIsConsistent && newIx == minputAddrIx
+                              , ifThenElse (isOnline inputAddrType)
+                                  (inputAddr .: inputsAccAddresses)
+                                  (inputsAccAddresses)
+                              )
+                              (inputsAccIsConsistent, inputsAccAddresses)
+                      in (newIx, newIsConsistent, newAddresses, ownerAddr)
+                  ) (txAccIx, txAccIsConsistent, emptyList :: List s (Address s), txOwner tx) (txInputs tx)
+                in (resInputsAccIx, resInputsAccIsConsistent, txAccAddresses ++ resInputsAccAddresses)
+        ) (nothing :: Maybe context (DAIndex context), true :: Bool context, emptyList :: List context (Address context)) tbdwTransactions
+  in
+      resTxAccIsConsistent
+   && (tbdAddresses == removeDuplicates resTxAccAddresses)
+
+-- TODO: Use generic 'elem' from symbolic list module.
+-- | Check if an item is present in the list.
+elem ::
+  forall context.
+  Signature context =>
+  Address context ->
+  List context (Address context) -> Bool context
+elem x xs =
+  fst $
+    Symbolic.List.foldl (
+      Morph \((acc :: Bool s, givenElement :: Address s), y :: Address s) ->
+        ( acc || givenElement == y
+        , givenElement
+        )
+    ) (false :: Bool context, x :: Address context) xs
+
+-- TODO: Use a generic function.
+-- | Remove duplicates from list.
+removeDuplicates ::
+  forall context.
+  Signature context =>
+  List context (Address context) -> List context (Address context)
+removeDuplicates ls =
+  Symbolic.List.foldr (Morph \(l :: Address s, acc :: List s (Address s)) ->
+          ifThenElse (elem l acc)
+            acc
+            (l .: acc)
+        ) emptyList ls
