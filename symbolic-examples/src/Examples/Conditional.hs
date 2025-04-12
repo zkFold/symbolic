7168d291c394597fe9f4907e9554eb98dd5fdb72
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2025-04-12 18:51:07 +0200

    Add some basic examples for benchmarking

--- symbolic-examples/src/Examples/Conditional.hs
+++ symbolic-examples/src/Examples/Conditional.hs
@@ -2,9 +2,9 @@ module Examples.Conditional (exampleConditional) where
 
 import           ZkFold.Symbolic.Class             (Symbolic)
 import           ZkFold.Symbolic.Data.Bool         (Bool)
-import           ZkFold.Symbolic.Data.Conditional  (Conditional (..))
+import           ZkFold.Symbolic.Data.Conditional  (ifThenElse)
 import           ZkFold.Symbolic.Data.FieldElement (FieldElement)
 
 exampleConditional ::
-  Symbolic c => FieldElement c -> FieldElement c -> Bool c -> FieldElement c
-exampleConditional = bool
+  Symbolic c => Bool c -> FieldElement c -> FieldElement c -> FieldElement c
+exampleConditional = ifThenElse
--- symbolic-examples/src/Examples/Constant.hs
+++ symbolic-examples/src/Examples/Constant.hs
@@ -1,15 +1,36 @@
-module Examples.Constant (exampleConst5, exampleEq5) where
+module Examples.Constant
+    ( exampleConst
+    , exampleConditionalConst
+    , exampleConditionalConstConst
+    , exampleEqConst
+    , exampleEqVectorConst) where
 
-import           Examples.Eq                       (exampleEq)
+import           Data.Function                     (const, ($))
+import           Examples.Conditional              (exampleConditional)
+import           Examples.Eq                       (exampleEq, exampleEqVector)
 
 import           ZkFold.Base.Algebra.Basic.Class   (FromConstant (..))
-import           ZkFold.Base.Algebra.Basic.Number  (Natural)
+import           ZkFold.Base.Algebra.Basic.Number  (Natural, KnownNat)
+import           ZkFold.Base.Data.Vector           (Vector)
 import           ZkFold.Symbolic.Class             (Symbolic)
 import           ZkFold.Symbolic.Data.Bool         (Bool)
 import           ZkFold.Symbolic.Data.FieldElement (FieldElement)
+import Control.Monad.Representable.Reader (Representable(tabulate))
 
-exampleConst5 :: Symbolic c => FieldElement c
-exampleConst5 = fromConstant (5 :: Natural)
 
-exampleEq5 :: Symbolic c => FieldElement c -> Bool c
-exampleEq5 = exampleEq exampleConst5
+exampleConst :: Symbolic c => FieldElement c
+exampleConst = fromConstant (5 :: Natural)
+
+exampleConditionalConst :: Symbolic c => Bool c -> FieldElement c -> FieldElement c
+exampleConditionalConst b x = exampleConditional b x exampleConst
+
+exampleConditionalConstConst :: Symbolic c => Bool c -> FieldElement c
+exampleConditionalConstConst b = exampleConditional b (fromConstant @Natural 3) exampleConst
+
+exampleEqConst :: Symbolic c => FieldElement c -> Bool c
+exampleEqConst = exampleEq exampleConst
+
+exampleEqVectorConst :: (KnownNat n, Symbolic c)
+    => Vector n (FieldElement c)
+    -> Bool c
+exampleEqVectorConst = exampleEqVector (tabulate $ const exampleConst)
