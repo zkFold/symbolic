0f0e4ea2882c9110e35c073e1cf7f7edd08bfa2a
Vladimir <me.morozovvp@gmail.com>
2025-05-26 22:11:39 +1000

    Split files for different architectures

--- symbolic-base/src/ZkFold/Protocol/NonInteractiveProof.hs
+++ symbolic-base/src/ZkFold/Protocol/NonInteractiveProof.hs
@@ -1,7 +1,16 @@
+{-# LANGUAGE CPP #-}
+
 module ZkFold.Protocol.NonInteractiveProof (
-    module ZkFold.Protocol.NonInteractiveProof.Internal,
-    module ZkFold.Protocol.NonInteractiveProof.Testing
+    module ZkFold.Protocol.NonInteractiveProof.Class,
+    module ZkFold.Protocol.NonInteractiveProof.Testing,
 ) where
 
-import           ZkFold.Protocol.NonInteractiveProof.Internal
+import           ZkFold.Protocol.NonInteractiveProof.Class
+#if defined(javascript_HOST_ARCH)
+import           ZkFold.Protocol.NonInteractiveProof.JS () 
+#elif defined(wasm32_HOST_ARCH)
+import           ZkFold.Protocol.NonInteractiveProof.WASM () 
+#else
+import           ZkFold.Protocol.NonInteractiveProof.Haskell () 
+#endif
 import           ZkFold.Protocol.NonInteractiveProof.Testing
