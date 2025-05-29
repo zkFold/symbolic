50d9c1443ecf58c260502a73535fbfd72f32ae35
TurtlePU <sokolov.p64@gmail.com>
2025-05-29 23:39:04 +0300

    + Empty typeclass

---
+++ symbolic-base/src/ZkFold/Data/Empty.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE TypeOperators #-}
+
+module ZkFold.Data.Empty where
+
+import           Data.Function    (const, ($))
+import           Data.Functor.Rep (Representable, tabulate)
+import           GHC.Generics     (U1 (..), (:*:) (..), (:.:) (..))
+
+-- TODO: Most likely temporary.
+-- Remove once we get rid of Layout/Payload split in SymbolicData.
+
+class Empty f where
+    empty :: f a
+
+instance Empty U1 where
+    empty = U1
+
+instance (Empty f, Empty g) => Empty (f :*: g) where
+    empty = empty :*: empty
+
+instance (Representable f, Empty g) => Empty (f :.: g) where
+    empty = Comp1 $ tabulate (const empty)
