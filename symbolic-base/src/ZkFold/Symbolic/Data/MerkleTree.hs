de94de79f1084ff5dbbc376684d275295023d411
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2025-08-05 10:44:53 +0200

    Merkle tree base implementation

--- symbolic-base/src/ZkFold/Symbolic/Data/MerkleTree.hs
+++ symbolic-base/src/ZkFold/Symbolic/Data/MerkleTree.hs
@@ -5,41 +5,36 @@
 
 module ZkFold.Symbolic.Data.MerkleTree where
 
-import Data.Constraint (withDict)
-import Data.Constraint.Nat (minusNat)
-import qualified Data.List as LL
+import Data.Bool (Bool)
+import Data.Eq (Eq (..))
+import Data.Function ((.))
+import Data.Functor ((<$>))
+import qualified Data.List as List
+import Data.Maybe (Maybe (..))
 import Data.Type.Equality (type (~))
+import qualified Data.Vector as V
 import GHC.Generics hiding (Rep, UInt, from)
 import GHC.TypeNats
+import Test.QuickCheck (Arbitrary (..))
 import ZkFold.Algebra.Class
-import ZkFold.Algebra.Number (integral, value)
-import ZkFold.Data.Package
+import ZkFold.Algebra.Field (Zp, fromZp, toZp)
 import ZkFold.Data.Vector hiding (zip, (.:))
 import qualified ZkFold.Data.Vector as V
 import ZkFold.Symbolic.Algorithm.Hash.MiMC
-import ZkFold.Symbolic.Class
-import ZkFold.Symbolic.Data.Bool (Bool (..))
-import ZkFold.Symbolic.Data.Class
-import ZkFold.Symbolic.Data.Combinators (
-  Iso (from),
-  RegisterSize (Auto),
-  expansion,
-  horner,
-  withNumberOfRegisters,
- )
-import ZkFold.Symbolic.Data.FieldElement (FieldElement (FieldElement, fromFieldElement))
-import ZkFold.Symbolic.Data.Input (SymbolicInput)
-import ZkFold.Symbolic.Data.Maybe
-import ZkFold.Symbolic.Data.Morph
-import ZkFold.Symbolic.Fold (SymbolicFold)
-import Prelude (error, pure, return, ($), (.))
-import qualified Prelude as P
+import ZkFold.Symbolic.Data.Combinators (Iso (from))
+import Prelude (Show, error, fromIntegral, pure, ($))
+
+type MerkleTreeSize d = 2 ^ (d - 1)
 
 data MerkleTree (d :: Natural) h = MerkleTree
   { mHash :: h
-  , mLeaves :: Vector (2 ^ (d - 1)) h
+  , mLeaves :: Vector (MerkleTreeSize d) h
   }
-  deriving Generic
+  deriving (Generic, Show, Eq)
+
+merkleHash
+  :: forall h. Ring h => h -> h -> h
+merkleHash = mimcHash2 mimcConstants zero
 
 -- | Computes the next level up in the merkle tree by pairing adjacent elements
 computeNextLevel
@@ -62,184 +57,92 @@ computeAllLevels leaves = go (fromVector leaves)
     let nextLevel = computeNextLevel current
      in current : go nextLevel
 
+computeRoot
+  :: forall d h
+   . Ring h
+  => Vector (MerkleTreeSize d) h
+  -> h
+computeRoot leaves =
+  case List.last (computeAllLevels leaves) of
+    [root] -> root
+    _ -> error "Invalid tree structure"
+
 zeroMerkleTree
   :: forall d h
-   . ( KnownNat (2 ^ (d - 1))
+   . ( KnownNat (MerkleTreeSize d)
      , Ring h
      )
   => MerkleTree d h
 zeroMerkleTree = MerkleTree rootHash leaves
  where
   leaves = pure zero :: Vector (2 ^ (d - 1)) h
-  allLevels = computeAllLevels leaves
-  rootHash = case P.last allLevels of
-    [root] -> root
-    _ -> error "Invalid tree structure"
+  rootHash = computeRoot leaves
 
 instance
-  forall c h d n
-   . ( SymbolicData h
-     , Context h ~ c
-     , Payload h ~ U1
-     , KnownNat d
-     , KnownNat n
-     , 2 ^ (d - 1) ~ n
-     , 1 <= d
+  forall d n h
+   . ( MerkleTreeSize d ~ n
      , Ring h
      )
   => Iso (Vector n h) (MerkleTree d h)
   where
-  from v = MerkleTree rootHash v
-   where
-    allLevels = computeAllLevels v
-    rootHash = case P.last allLevels of
-      [root] -> root
-      _ -> error "Invalid tree structure"
+  from v = MerkleTree (computeRoot v) v
 
 instance
-  forall c h d n
-   . ( SymbolicData h
-     , Context h ~ c
-     , Payload h ~ U1
-     , KnownNat d
-     , KnownNat n
-     , 2 ^ (d - 1) ~ n
-     , 1 <= d
+  forall d n h
+   . ( MerkleTreeSize d ~ n
      , Ring h
      )
   => Iso (MerkleTree d h) (Vector n h)
   where
   from (MerkleTree _ leaves) = leaves
 
-merkleHash
-  :: forall h. Ring h => h -> h -> h
-merkleHash = mimcHash2 mimcConstants zero
-
-instance (SymbolicData h, KnownNat d, KnownNat (2 ^ (d - 1))) => SymbolicData (MerkleTree d h)
-
-instance (SymbolicInput h, KnownNat d, KnownNat (2 ^ (d - 1))) => SymbolicInput (MerkleTree d h)
+instance (KnownNat (MerkleTreeSize d), Ring h, Arbitrary h) => Arbitrary (MerkleTree d h) where
+  arbitrary = from @(Vector (MerkleTreeSize d) h) <$> arbitrary
 
 -- | Finds an element satisfying the constraint
 find
-  :: forall c h d
-   . ( SymbolicInput h
-     , Context h ~ c
-     , SymbolicFold c
-     )
-  => MorphFrom c h (Bool c)
+  :: forall d h
+   . (h -> Bool)
   -> MerkleTree d h
-  -> Maybe c h
-find p MerkleTree {..} =
-  let leaves = fromVector mLeaves
-      matches = P.filter (\x -> evalBool (p @ x)) leaves
-   in case matches of
-        [] -> nothing
-        (x : _) -> just x
- where
-  evalBool :: Bool c -> P.Bool
-  evalBool _ = P.True -- Simplified for now
+  -> Maybe h
+find p (MerkleTree _ (Vector leaves)) = V.find p leaves
 
-newtype MerkleTreePath (d :: Natural) c = MerkleTreePath {mPath :: Vector (d - 1) (Bool c)}
-  deriving Generic
+-- | Finds an index of an element satisfying the constraint
+findIndex
+  :: forall d h
+   . KnownNat (MerkleTreeSize d)
+  => (h -> Bool)
+  -> MerkleTree d h
+  -> Maybe (Zp (MerkleTreeSize d))
+findIndex p (MerkleTree _ (Vector leaves)) = toZp . fromIntegral <$> V.findIndex p leaves
 
-instance (Symbolic c, KnownNat (d - 1)) => SymbolicData (MerkleTreePath d c)
+-- | Returns the index of the first occurrence of an element in the Merkle tree
+elemIndex
+  :: forall d h
+   . (KnownNat (MerkleTreeSize d), Eq h)
+  => h
+  -> MerkleTree d h
+  -> Maybe (Zp (MerkleTreeSize d))
+elemIndex h = findIndex (== h)
 
--- | Finds a path to an element satisfying the constraint
-findPath
-  :: forall x c d
-   . ( SymbolicData x
-     , Context x ~ c
-     , SymbolicFold c
-     , KnownNat d
-     , 1 <= d
-     )
-  => MorphFrom c x (Bool c)
-  -> MerkleTree d x
-  -> Maybe c (MerkleTreePath d c)
-findPath p (MerkleTree _ leaves) = withDict (minusNat @d @1) $
-  case LL.findIndex (\x -> evalBool (p @ x)) (fromVector leaves) of
-    P.Nothing -> nothing
-    P.Just idx ->
-      just $ MerkleTreePath . P.fmap Bool . indToPath @c . fromFieldElement . fromConstant $ (P.fromIntegral idx :: Natural)
- where
-  evalBool :: Bool c -> P.Bool
-  evalBool _ = P.True -- Simplified for now
-
-indToPath :: forall c d. (Symbolic c, KnownNat d) => c Par1 -> Vector (d - 1) (c Par1)
-indToPath e = unpack $ fromCircuitF e $ \(Par1 i) -> do
-  ee <- expansion (integral @d) i
-  return $ Comp1 (V.unsafeToVector @(d - 1) $ P.map Par1 ee)
-
--- | Returns the element corresponding to a path
+-- | Returns the element at the given index in the Merkle tree
 lookup
-  :: forall x c d
-   . KnownNat d
-  => MerkleTree d x
-  -> MerkleTreePath d c
-  -> x
-lookup (MerkleTree _ leaves) (MerkleTreePath _) =
-  let leafIdx = (value @d -! 1) -- Simplified index calculation
-   in leaves V.!! leafIdx
+  :: forall d h
+   . MerkleTree d h
+  -> Zp (MerkleTreeSize d)
+  -> h
+lookup (MerkleTree _ leaves) idx = leaves V.!! fromZp idx
 
--- element by index (simplified)
-leaf
-  :: forall c x d
-   . Vector (2 ^ (d - 1)) x
-  -> c Par1
-  -> x
-leaf leaves _ = leaves V.!! 0 -- Simplified for now
-
--- index of element in path to element
-ind :: forall d c. Symbolic c => Vector d (c Par1) -> c Par1
-ind vb = fromCircuitF (pack vb) $ \vb' -> do
-  let bs = P.map unPar1 $ V.fromVector $ unComp1 vb'
-  b1n <- P.fmap Par1 . horner $ bs
-  return $ fromConstant b1n
-
--- | Inserts an element at a specified position in a tree
-insertLeaf
-  :: forall h c d
+-- | Replaces an element at a given index in the Merkle tree
+replaceAt
+  :: forall d h
    . Ring h
-  => MerkleTree d h
-  -> MerkleTreePath d c
+  => Zp (MerkleTreeSize d)
   -> h
   -> MerkleTree d h
-insertLeaf (MerkleTree _ leaves) (MerkleTreePath _) _ =
-  let newLeaves = leaves -- Simplified - should update at index
-      allLevels = computeAllLevels newLeaves
-      newRoot = case P.last allLevels of
-        [root] -> root
-        _ -> error "Invalid tree structure"
-   in MerkleTree newRoot newLeaves
-
--- | Replaces an element satisfying the constraint. A composition of `findPath` and `insert`
-replace
-  :: forall h c d n
-   . ( SymbolicData h
-     , Context h ~ c
-     , SymbolicFold c
-     , KnownNat d
-     , KnownNat (2 ^ (d - 1))
-     , 1 <= d
-     , NumberOfBits (BaseField c) ~ n
-     , Ring h
-     )
-  => MorphFrom c h (Bool c)
   -> MerkleTree d h
-  -> h
-  -> MerkleTree d h
-replace p t h =
-  withNumberOfRegisters @n @Auto @(BaseField c) $
-    withDict (minusNat @d @1) $
-      maybe t (\path -> insertLeaf t path h) (findPath @h @c p t)
-
--- | Returns the next path in a tree
-incrementPath
-  :: forall c d
-   . ( KnownNat d
-     , Symbolic c
-     )
-  => MerkleTreePath d c
-  -> MerkleTreePath d c
-incrementPath (MerkleTreePath p) =
-  MerkleTreePath . P.fmap Bool . indToPath @c $ fromFieldElement (FieldElement (ind $ P.fmap (\(Bool b) -> b) p) + one)
+replaceAt idx newLeaf (MerkleTree _ leaves) =
+  MerkleTree root' leaves'
+ where
+  leaves' = Vector $ toV leaves V.// [(fromIntegral $ fromZp idx, newLeaf)]
+  root' = computeRoot leaves'
--- symbolic-base/test/Tests/Symbolic/Data/MerkleTree.hs
+++ symbolic-base/test/Tests/Symbolic/Data/MerkleTree.hs
@@ -6,185 +6,88 @@ module Tests.Symbolic.Data.MerkleTree (
   specMerkleTree,
 ) where
 
-import Data.Binary (Binary)
-import Data.Type.Equality (type (~))
-import GHC.Generics (Par1 (Par1), U1 (..), type (:*:) ((:*:)))
-import GHC.TypeNats (type (-), type (<=), type (^))
+import Data.Bool (Bool (..))
+import Data.Eq (Eq (..))
+import Data.Maybe (Maybe (..))
+import GHC.TypeNats (type (-), type (^))
 import Test.Hspec (Spec, describe)
-import Test.Hspec.QuickCheck (prop)
-import Test.QuickCheck (Arbitrary, Gen, arbitrary, vectorOf)
-import ZkFold.Algebra.Class (Ring, fromConstant, one, (-!), (^))
+import Test.QuickCheck (Arbitrary)
+import Tests.Common (it)
+import ZkFold.Algebra.Class (Ring, toConstant)
 import ZkFold.Algebra.EllipticCurve.BLS12_381 (BLS12_381_Scalar)
 import ZkFold.Algebra.Field (Zp)
-import ZkFold.Algebra.Number (KnownNat, Natural, value)
-import ZkFold.ArithmeticCircuit (eval1)
-import ZkFold.Data.Eq
-import ZkFold.Data.Vector (Vector (..))
-import qualified ZkFold.Data.Vector as V
-import ZkFold.Prelude (chooseNatural)
-import ZkFold.Symbolic.Class
-import ZkFold.Symbolic.Compiler (compile)
-import ZkFold.Symbolic.Data.Bool
-import ZkFold.Symbolic.Data.Class (Context, Payload, SymbolicData)
+import ZkFold.Algebra.Number (KnownNat)
+import ZkFold.Data.Vector (Vector (..), (!!))
 import ZkFold.Symbolic.Data.Combinators (Iso (..))
-import ZkFold.Symbolic.Data.FieldElement (FieldElement (FieldElement))
-import ZkFold.Symbolic.Data.List (emptyList, lSize)
-import qualified ZkFold.Symbolic.Data.List as L
-import ZkFold.Symbolic.Data.Maybe (fromJust, nothing)
 import ZkFold.Symbolic.Data.MerkleTree
-import ZkFold.Symbolic.Data.Morph (MorphFrom, MorphTo (..))
-import ZkFold.Symbolic.Fold
-import ZkFold.Symbolic.Interpreter (Interpreter (..))
-import Prelude (fromIntegral, return, ($), (-), (.))
+import Prelude (Show, ($))
 import qualified Prelude as Haskell
 
--- evalBool :: forall a . (Arithmetic a, Binary a) => Bool (AC a) -> a
--- evalBool (Bool ac) = exec1 ac
-
-evalBoolVec :: forall a. Bool (Interpreter a) -> a
-evalBoolVec (Bool (Interpreter (Par1 v))) = v
-
--- Generic test for isomorphism between Vector and MerkleTree
 testId
-  :: forall c x d n
-   . ( SymbolicData x
-     , Context x ~ c
-     , Payload x ~ U1
-     , KnownNat d
-     , n ~ 2 ^ (d - 1)
-     , 1 <= d
-     , Eq x
-     , BooleanOf x ~ Bool c
-     , KnownNat n
-     , Ring x
+  :: forall d h
+   . ( Ring h
+     , Eq h
      )
-  => Vector n x
-  -> Bool c
-testId v = v == (from (from v :: MerkleTree d x) :: Vector n x)
-
-testPath :: forall d c. (Symbolic c, KnownNat d) => FieldElement c -> Bool c
-testPath fe@(FieldElement e) = fe == (FieldElement . ind $ indToPath @c @d e)
-
-testLayerFolding
-  :: forall c. SymbolicFold c => FieldElement c -> FieldElement c -> FieldElement c -> FieldElement c -> Bool c
-testLayerFolding x y z w =
-  let l = x L..: y L..: z L..: w L..: emptyList
-      m =
-        L.foldr
-          (Morph \(a :: FieldElement s, b :: L.List s (FieldElement s)) -> a L..: b)
-          (emptyList :: L.List c (FieldElement c))
-          l
-   in fromConstant (4 :: Natural)
-        == FieldElement (lSize l)
-        && fromConstant (4 :: Natural)
-        == FieldElement (lSize m)
+  => MerkleTree d h
+  -> Bool
+testId mt = mt == (from (from mt :: Vector (MerkleTreeSize d) h) :: MerkleTree d h)
 
 testFind
-  :: forall c
-   . SymbolicFold c
-  => BaseField c
-  -> BaseField c
-  -> FieldElement c
-  -> FieldElement c
-  -> FieldElement c
-  -> FieldElement c
-  -> Bool c
-testFind aw out x y z w =
-  let v = V.unsafeToVector [x, y, z, w]
-      mt = from v :: MerkleTree 3 (FieldElement c)
-      p :: BaseField c -> MorphFrom c (FieldElement c) (Bool c)
-      p t = Morph \(a :: FieldElement s) -> fromConstant t == a
-      fw = find (p aw) mt
-      fOut = find (p out) mt
-   in (fOut == nothing) && (fromConstant aw == fromJust fw)
+  :: forall d h
+   . Eq h
+  => MerkleTree d h
+  -> Zp (MerkleTreeSize d)
+  -> Bool
+testFind mt@(MerkleTree _ leaves) k =
+  let h = leaves !! toConstant k
+   in find (== h) mt == Just h
 
 testLookup
-  :: forall c
-   . SymbolicFold c
-  => FieldElement c
-  -> FieldElement c
-  -> FieldElement c
-  -> FieldElement c
-  -> Bool c
-testLookup x y z w =
-  let v = V.unsafeToVector [x, y, z, w] -- 4 elements: 2^(3-1) = 2^2 = 4
-      mt = from v :: MerkleTree 3 (FieldElement c)
-      -- Create a simple test path (first element, index 0)
-      path = MerkleTreePath (V.unsafeToVector [false, false] :: Vector 2 (Bool c))
-      result = lookup mt path
-   in -- For a simplified test, just check that lookup returns one of the elements
-      (result == x) || (result == y) || (result == z) || (result == w)
+  :: forall d h
+   . ( KnownNat (MerkleTreeSize d)
+     , Eq h
+     )
+  => MerkleTree d h
+  -> Zp (MerkleTreeSize d)
+  -> Haskell.Bool
+testLookup mt@(MerkleTree _ leaves) k =
+  let h = leaves !! toConstant k
+   in case elemIndex h mt of
+        Just idx -> lookup mt idx == h
+        Nothing -> False
 
-tossPow :: Natural -> Gen Natural
-tossPow m = chooseNatural (0, (2 :: Natural) ^ m -! 1)
-
--- genVec :: forall n. KnownNat n => Natural -> Gen (Vector n Natural)
--- genVec m = do
---   a <- replicateM (fromIntegral $ value @n) (tossPow m)
--- return $ V.unsafeToVector a
+testReplaceAt
+  :: forall d h
+   . ( Ring h
+     , Eq h
+     )
+  => Zp (MerkleTreeSize d)
+  -> h
+  -> MerkleTree d h
+  -> Bool
+testReplaceAt k h mt = lookup (replaceAt k h mt) k == h
 
 specMerkleTree'
   :: forall a d
    . ( Arbitrary a
-     , Arithmetic a
-     , Binary a
-     , Haskell.Show a
-     , KnownNat d
+     , Ring a
+     , Eq a
+     , Show a
      , KnownNat (2 ^ (d - 1))
-     , 1 <= d
      )
   => Spec
 specMerkleTree' = do
-  describe "MerkleTree specification" $ do
-    prop "testId" $ do
-      -- Generate a vector of length 2^(d-1)
-      let n = 2 ^ (value @d - 1) :: Natural
-      elements <- vectorOf (fromIntegral n) (arbitrary :: Gen a)
-      let v = V.unsafeToVector (Haskell.map fromConstant elements) :: Vector (2 ^ (d - 1)) (FieldElement (Interpreter a))
-          result = testId @(Interpreter a) @(FieldElement (Interpreter a)) @d @(2 ^ (d - 1)) v
-      return $ evalBoolVec result Haskell.== one
-    prop "testPath" $ do
-      r <- tossPow (value @d)
-      let ac = eval1 (compile @a $ testPath @d) ((U1 :*: U1) :*: Par1 (fromConstant r) :*: U1)
-          i = evalBoolVec $ testPath @d (fromConstant r)
-      return $ ac Haskell.== i && (i Haskell.== one)
-    prop "testLayerFolding" $ \x y z w ->
-      let ac =
-            eval1
-              (compile @a testLayerFolding)
-              ((U1 :*: U1 :*: U1 :*: U1 :*: U1) :*: Par1 x :*: Par1 y :*: Par1 z :*: Par1 w :*: U1)
-          i = evalBoolVec $ testLayerFolding (fromConstant x) (fromConstant y) (fromConstant z) (fromConstant w)
-       in ac Haskell.== i && (i Haskell.== one)
-    prop "testLookup" $ \x y z w ->
-      let ac = eval1 (compile @a testLookup) ((U1 :*: U1 :*: U1 :*: U1 :*: U1) :*: Par1 x :*: Par1 y :*: Par1 z :*: Par1 w :*: U1)
-          i = evalBoolVec $ testLookup (fromConstant x) (fromConstant y) (fromConstant z) (fromConstant w)
-       in ac Haskell.== i && (i Haskell.== one)
-    prop "testFind" $ \x y z w -> do
-      aw <- tossPow 8 :: Gen Natural
-      out <- tossPow 8 :: Gen Natural
-      let ac =
-            eval1
-              (compile @a (testFind (fromConstant aw) (fromConstant out)))
-              ((U1 :*: U1 :*: U1 :*: U1 :*: U1) :*: Par1 x :*: Par1 y :*: Par1 z :*: Par1 w :*: U1)
-          (xf, yf, zf, wf) = (fromConstant x, fromConstant y, fromConstant z, fromConstant w)
-          i = evalBoolVec $ testFind (fromConstant aw) (fromConstant out) xf yf zf wf
-      return $ ac Haskell.== i
-
--- prop "testLookup" $ \x y z w -> do
---   r <- tossPow d
---   let ac = eval1 (compile @a testLookup) ((U1 :*: U1 :*: U1 :*: U1 :*: U1 :*: U1) :*: Par1 (fromConstant r) :*: Par1 x :*: Par1 y :*: Par1 z :*: Par1 w :*: U1)
---       i  = evalBoolVec $ testLookup (fromConstant r) (fromConstant x) (fromConstant y) (fromConstant z) (fromConstant w)
---   return $ (ac, i) === (i, one)
--- -- TODO: benchmark this test and optimize find
--- prop "testFind" $ \x y z -> do
---   w   <- fromConstant <$> tossPow d :: Gen a
---   out <- fromConstant <$> tossPow d :: Gen a
---   let ac = eval1 (compile @a (testFind w out)) ((U1 :*: U1 :*: U1 :*: U1 :*: U1) :*: Par1 x :*: Par1 y :*: Par1 z :*: Par1 w :*: U1)
---       (xf, yf, zf, wf, outf) = (fromConstant x ::  FieldElement (I a), fromConstant y, fromConstant z, fromConstant w, fromConstant out)
---       i  = evalBoolVec $ testFind w out xf yf zf wf
---       c  = evalBoolVec $ (outf /= xf) && (outf /= yf) && (outf /= zf) && (outf /= wf)
---   return $ xor (ac Haskell.== i) (i Haskell.== one) `xor` (c Haskell.== one)
+  describe "MerkleTree specification" $
+    describe "Merkle tree" $ do
+      it "is isomorphic to Vector" $ \mt ->
+        testId @d @a mt
+      it "finds an element in the Merkle tree" $ \mt k ->
+        testFind @d @a mt k
+      it "Looks up an element in the Merkle tree" $ \mt k ->
+        testLookup @d @a mt k
+      it "replaces an element in the Merkle tree" $ \mt k h ->
+        testReplaceAt @d @a k h mt
 
 specMerkleTree :: Spec
 specMerkleTree = do
-  specMerkleTree' @(Zp BLS12_381_Scalar) @8
+  specMerkleTree' @(Zp BLS12_381_Scalar) @3
