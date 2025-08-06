3b07eb41990a8131154a1a5d4d558bf34a26b36c
diS3e <diS3e@users.noreply.github.com>
2025-08-06 08:11:16 +0000

    fourmolu auto-commit

--- symbolic-base/src/ZkFold/FFI/Rust/RustBLS.hs
+++ symbolic-base/src/ZkFold/FFI/Rust/RustBLS.hs
@@ -12,7 +12,6 @@ import qualified Data.Vector as V
 import GHC.Base
 import GHC.Natural (naturalFromInteger)
 import Test.QuickCheck hiding (scale)
-import ZkFold.FFI.Rust.Runner (runRustFun0, runRustFun1, runRustFun2, runRustFun3)
 import Prelude hiding (fromIntegral, negate, sum, (*), (+), (-), (^))
 import qualified Prelude as P
 
@@ -31,6 +30,7 @@ import ZkFold.Data.Binary
 import qualified ZkFold.Data.Eq
 import ZkFold.FFI.Rust.Conversion
 import ZkFold.FFI.Rust.Poly ()
+import ZkFold.FFI.Rust.Runner (runRustFun0, runRustFun1, runRustFun2, runRustFun3)
 import ZkFold.FFI.Rust.RustFunctions
 import ZkFold.FFI.Rust.Types
 import ZkFold.Symbolic.MonadCircuit
