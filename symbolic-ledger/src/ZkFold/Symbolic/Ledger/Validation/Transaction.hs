0864a8157e3b4505211ec0ade66863ea626c0545
sourabhxyz <sourabhxyz@users.noreply.github.com>
2025-09-23 13:55:14 +0000

    fourmolu auto-commit

--- symbolic-ledger/src/ZkFold/Symbolic/Ledger/Validation/Transaction.hs
+++ symbolic-ledger/src/ZkFold/Symbolic/Ledger/Validation/Transaction.hs
@@ -29,8 +29,9 @@ import ZkFold.Symbolic.Data.Hash (hash)
 import qualified ZkFold.Symbolic.Data.Hash as Base
 import ZkFold.Symbolic.Data.MerkleTree (MerkleEntry, MerkleTree)
 import qualified ZkFold.Symbolic.Data.MerkleTree as MerkleTree
-import ZkFold.Symbolic.Ledger.Types
 import qualified Prelude as P
+
+import ZkFold.Symbolic.Ledger.Types
 
 -- | Transaction witness for validating transaction.
 data TransactionWitness ud i o a context = TransactionWitness
@@ -234,8 +235,7 @@ outputHasAtLeastOneAda output =
   foldl'
     ( \found asset ->
         found
-          || ( asset.assetPolicy == adaPolicy && asset.assetName == adaName && asset.assetQuantity >= fromConstant @P.Integer 1_000_000
-             )
+          || (asset.assetPolicy == adaPolicy && asset.assetName == adaName && asset.assetQuantity >= fromConstant @P.Integer 1_000_000)
     )
     false
     (unComp1 (oAssets output))
