445523df5aed1e39df15e914501ba3b42fb23022
sourabhxyz <lambdaproofs@pm.me>
2025-09-25 19:58:40 +0530

    feat(#707): add eddsa

---
+++ symbolic-base/src/ZkFold/Symbolic/Algorithm/EdDSA.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+
+module ZkFold.Symbolic.Algorithm.EdDSA (
+  eddsaVerify,
+) where
+
+import Data.Coerce (coerce)
+import Data.Type.Equality
+import GHC.Generics ((:*:) (..))
+import ZkFold.Algebra.Class
+import ZkFold.Algebra.EllipticCurve.Class hiding (AffinePoint, Point)
+import qualified ZkFold.Algebra.EllipticCurve.Class as Elliptic
+import ZkFold.Control.Conditional (ifThenElse)
+import ZkFold.Data.Eq
+import ZkFold.Symbolic.Data.Bool
+import qualified ZkFold.Symbolic.Data.EllipticCurve.Point.Affine as SymAffine
+
+-- | Verify EdDSA signature on a Twisted Edwards curve.
+--
+-- It checks the standard relation:  s*G == R + H(R, A, M) * A
+-- where:
+--   - A is the public key (point)
+--   - (R, s) is the signature; R is a point, s is a scalar
+--   - H is a caller-provided hash-to-scalar function
+eddsaVerify
+  :: forall message point curve baseField scalarField ctx
+   . ( point ~ SymAffine.AffinePoint (TwistedEdwards curve) baseField ctx
+     , ScalarFieldOf point ~ scalarField ctx
+     , CyclicGroup point
+     , Zero (scalarField ctx)
+     , Eq (scalarField ctx)
+     , BooleanOf (scalarField ctx) ~ Bool ctx
+     , Semiring (baseField ctx)
+     , Eq (baseField ctx)
+     , BooleanOf (baseField ctx) ~ Bool ctx
+     )
+  => ( point
+       -> point
+       -> message
+       -> scalarField ctx
+     )
+  -> point
+  -- ^ public key A
+  -> message
+  -- ^ message M
+  -> (SymAffine.AffinePoint (TwistedEdwards curve) baseField :*: scalarField) ctx
+  -- ^ signature (R, s)
+  -> Bool ctx
+eddsaVerify hashFn publicKey message (rPoint :*: s) =
+  if isIdentity rPoint || s == zero
+    then false
+    else unwrap lhs == unwrap rhs -- `unwrap` as `Eq` instance is missing.
+ where
+  g = pointGen @point
+
+  h = hashFn rPoint publicKey message
+
+  lhs = s `scale` g
+
+  rhs = rPoint + h `scale` publicKey
+
+  unwrap :: point -> Elliptic.AffinePoint (baseField ctx)
+  unwrap = coerce
+
+  isIdentity :: point -> Bool ctx
+  isIdentity p = unwrap p == Elliptic.pointXY zero one
