a44e7b2e0ed30e62b3ee9430b0f62432847ebbef
sourabhxyz <lambdaproofs@pm.me>
2025-09-26 19:24:04 +0530

    feat(#707): remove poseidon variant for ecdsaVerfiy

--- symbolic-base/src/ZkFold/Symbolic/Algorithm/ECDSA/ECDSA.hs
+++ symbolic-base/src/ZkFold/Symbolic/Algorithm/ECDSA/ECDSA.hs
@@ -9,11 +9,8 @@ module ZkFold.Symbolic.Algorithm.ECDSA.ECDSA where
 import Data.Type.Equality
 import GHC.Generics ((:*:) (..))
 import GHC.TypeLits (KnownNat)
-import qualified Prelude as P
-
 import ZkFold.Algebra.Class hiding (Euclidean (..))
 import ZkFold.Algebra.EllipticCurve.Class hiding (Point)
-import qualified ZkFold.Algorithm.Hash.Poseidon as Poseidon
 import ZkFold.Control.Conditional (ifThenElse)
 import ZkFold.Data.Eq
 import qualified ZkFold.Symbolic.Class as S
@@ -22,6 +19,7 @@ import ZkFold.Symbolic.Data.Combinators (GetRegisterSize, NumberOfRegisters, Reg
 import ZkFold.Symbolic.Data.EllipticCurve.Point (Point (..))
 import ZkFold.Symbolic.Data.FFA (FFA, KnownFFA, toUInt)
 import ZkFold.Symbolic.Data.UInt (UInt)
+import qualified Prelude as P
 
 -- Verify ECDSA where a caller-provided hash function maps a message to it's hash.
 ecdsaVerify
@@ -59,27 +57,6 @@ ecdsaVerify hashFn publicKey message (r :*: s) =
   u2 = r * sInv
 
   c = u1 `scale` g + u2 `scale` publicKey
-
--- | Verify ECDSA where the message is hashed using Poseidon.
-ecdsaVerifyPoseidon
-  :: forall n point curve p q baseField scalarField ctx
-   . ( S.Symbolic ctx
-     , baseField ~ FFA q 'Auto
-     , scalarField ~ FFA p 'Auto
-     , point ~ Point (Weierstrass curve) baseField ctx
-     , ScalarFieldOf point ~ scalarField ctx
-     , CyclicGroup point
-     , KnownFFA q 'Auto ctx
-     , KnownFFA p 'Auto ctx
-     , KnownNat n
-     , KnownNat (NumberOfRegisters (S.BaseField ctx) n 'Auto)
-     , KnownNat (GetRegisterSize (S.BaseField ctx) n 'Auto)
-     )
-  => point
-  -> [scalarField ctx]
-  -> (scalarField :*: scalarField) ctx
-  -> Bool ctx
-ecdsaVerifyPoseidon = ecdsaVerify @_ @n Poseidon.poseidonHashDefault
 
 -- | Variant of 'ecdsaVerify' where the message is already hashed.
 ecdsaVerifyMessageHash
