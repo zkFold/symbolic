ae05f5f15e802920b6e2bd3cd988afa96a5f97d3
TurtlePU <sokolov.p64@gmail.com>
2025-10-02 18:06:45 +0300

    WIP almost finished with node compilation

--- symbolic-base/src/ZkFold/ArithmeticCircuit/Experimental.hs
+++ symbolic-base/src/ZkFold/ArithmeticCircuit/Experimental.hs
@@ -12,7 +12,7 @@ module ZkFold.ArithmeticCircuit.Experimental where
 
 import Control.Applicative (Applicative, pure, (<*>))
 import Control.DeepSeq (NFData (..), rwhnf)
-import Control.Monad (unless)
+import Control.Monad (unless, (>>=))
 import Control.Monad.State (State, gets, modify', runState)
 import Data.Bifunctor (first)
 import Data.Binary (Binary)
@@ -20,7 +20,7 @@ import Data.Bool (Bool (..))
 import Data.ByteString (ByteString)
 import qualified Data.Eq as Prelude
 import Data.Function (const, flip, ($), (.))
-import Data.Functor (Functor, fmap, (<$>))
+import Data.Functor (Functor, fmap, (<$>), (<&>))
 import Data.Kind (Type)
 import Data.Map (Map)
 import qualified Data.Map as M
@@ -55,6 +55,8 @@ import ZkFold.Symbolic.Compiler ()
 import ZkFold.Symbolic.Data.V2 (HasRep, Layout, SymbolicData (fromLayout, toLayout))
 import ZkFold.Symbolic.MonadCircuit (MonadCircuit (constraint, lookupConstraint))
 import ZkFold.Symbolic.V2 (Constraint (..), Symbolic (..))
+import Data.Maybe (Maybe(..))
+import Unsafe.Coerce (unsafeCoerce)
 
 ------------------- Experimental single-output circuit type --------------------
 
@@ -319,8 +321,18 @@ compileNode (NodeConstrain c n h) = do
       poly <- traversePoly @_ @a (fmap toVar . compileNode) p
       zoom #circuitContext $ constraint (evalPoly poly)
   compileNode n
-compileNode (NodeApply op h) = do
-  _
+compileNode (NodeApply op h) = gets (request op . witnessExtractor) >>= \case
+  Just w -> pure w
+  Nothing -> do
+    w <- traverseOp compileNode op >>= \case
+      OpConst c -> _
+    zoom #witnessExtractor $ modify' $ M.insert h (SomeWitness w)
+    pure w
+  where
+    request :: Op f t -> Map Hash (SomeWitness a) -> Maybe (Witness a t)
+    request _ m = (m M.!? h) <&> \case
+      SomeWitness s -> unsafeCoerce s
+      -- ^ safe assuming no collisions
 
 --    (Just w, EqVar bs) -> do
 --      isDone <- gets (M.member bs . acWitness)
