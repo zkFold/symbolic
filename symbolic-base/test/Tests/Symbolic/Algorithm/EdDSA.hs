c84de449337b228655678a05aac625085f82db67
sourabhxyz <lambdaproofs@pm.me>
2025-10-10 17:03:44 +0200

    feat(#721): use property based test for eddsaVerify

--- symbolic-base/test/Tests/Symbolic/Algorithm/EdDSA.hs
+++ symbolic-base/test/Tests/Symbolic/Algorithm/EdDSA.hs
@@ -3,21 +3,23 @@
 module Tests.Symbolic.Algorithm.EdDSA (specEdDSA) where
 
 import Data.Function (($))
+import Data.Functor ((<$>))
 import GHC.Generics ((:*:) (..))
 import Test.Hspec (Spec, describe)
-import Test.QuickCheck ((.&.), (===))
-import Prelude ()
+import Test.QuickCheck (counterexample, forAll, (.&.), (===))
+import Text.Show (show)
+import Prelude ((<>))
 
-import Tests.Common (evalBool, it)
+import Tests.Common (evalBool, it, toss, toss1)
 import ZkFold.Algebra.Class
 import ZkFold.Algebra.EllipticCurve.BLS12_381 (Fr)
 import ZkFold.Algebra.EllipticCurve.Jubjub (Jubjub_Base, Jubjub_Scalar)
 import ZkFold.Algebra.Number
 import ZkFold.Symbolic.Algorithm.EdDSA (eddsaVerify)
-import ZkFold.Symbolic.Algorithm.Hash.Poseidon ()
 import ZkFold.Symbolic.Data.Combinators (RegisterSize (Auto), from)
 import ZkFold.Algebra.EllipticCurve.Class (pointGen)
 import ZkFold.Symbolic.Data.EllipticCurve.Jubjub (Jubjub_Point)
+import qualified ZkFold.Symbolic.Data.EllipticCurve.Point.Affine as SymAffine
 import ZkFold.Symbolic.Data.FFA (FFA, KnownFFA, fromUInt)
 import ZkFold.Symbolic.Data.FieldElement (FieldElement)
 import ZkFold.Symbolic.Interpreter (Interpreter)
@@ -33,28 +35,31 @@ specEdDSA :: Spec
 specEdDSA = describe "EdDSA verification (Jubjub, Poseidon Hash)" $ do
   it "verifies a correctly formed signature" $ do
     let g = pointGen @Point
-        x = fromConstant (7 :: Natural) :: Scalar -- private key
-        k = fromConstant (11 :: Natural) :: Scalar -- nonce
-        a = x `scale` g
-        r = k `scale` g
-        m = zero :: FieldElement I
-        h = hashToScalar r a m
-        s = k + h * x
-        ok = eddsaVerify hashToScalar a m (r :*: s)
-    evalBool ok === one
+        p = value @Jubjub_Scalar
+    forAll (fromConstant <$> toss p) $ \(x :: Scalar) ->
+      forAll (fromConstant <$> toss1 p) $ \(k :: Scalar) -> do
+        let a = x `scale` g
+            r = k `scale` g
+            m = zero :: FieldElement I
+            h = hashToScalar r a m
+            s = k + h * x
+            ok = eddsaVerify hashToScalar a m (r :*: s)
+            rAffine = SymAffine.affinePoint r
+        counterexample ("r = " <> show rAffine <> "\ns = " <> show s) $ evalBool ok === one
 
   it "rejects tampered signatures and degenerate inputs" $ do
     let g = pointGen @Point
-        x = fromConstant (5 :: Natural) :: Scalar
-        k = fromConstant (3 :: Natural) :: Scalar
-        a = x `scale` g
-        r = k `scale` g
-        m = zero :: FieldElement I
-        h = hashToScalar r a m
-        s = k + h * x
-        wrongS = s + one
-        badWrongS = eddsaVerify hashToScalar a m (r :*: wrongS)
-        badSZero = eddsaVerify hashToScalar a m (r :*: zero)
-    evalBool badWrongS === zero .&. evalBool badSZero === zero
+        p = value @Jubjub_Scalar
+    forAll (fromConstant <$> toss p) $ \(x :: Scalar) ->
+      forAll (fromConstant <$> toss1 p) $ \(k :: Scalar) -> do
+        let a = x `scale` g
+            r = k `scale` g
+            m = zero :: FieldElement I
+            h = hashToScalar r a m
+            s = k + h * x
+            wrongS = s + one
+            badWrongS = eddsaVerify hashToScalar a m (r :*: wrongS)
+            badSZero = eddsaVerify hashToScalar a m (r :*: zero)
+        evalBool badWrongS === zero .&. evalBool badSZero === zero
 
 
