fae8e1535741696ac7ffc0b7f98fa562248ad0f0
sourabhxyz <lambdaproofs@pm.me>
2025-10-10 17:36:05 +0200

    feat(#721): use MiMC hash temporarily (inplace of poseidon), have algorithm on signature computation

--- symbolic-base/test/Tests/Symbolic/Algorithm/EdDSA.hs
+++ symbolic-base/test/Tests/Symbolic/Algorithm/EdDSA.hs
@@ -23,29 +23,32 @@ import qualified ZkFold.Symbolic.Data.EllipticCurve.Point.Affine as SymAffine
 import ZkFold.Symbolic.Data.FFA (FFA, KnownFFA, fromUInt)
 import ZkFold.Symbolic.Data.FieldElement (FieldElement)
 import ZkFold.Symbolic.Interpreter (Interpreter)
+import qualified ZkFold.Symbolic.Algorithm.Hash.Poseidon as Poseidon
+import qualified ZkFold.Symbolic.Algorithm.Hash.MiMC as MiMC
 
 type I = Interpreter Fr
 type Point = Jubjub_Point I
 type Scalar = FFA Jubjub_Scalar 'Auto I
 
 hashToScalar :: (KnownFFA Jubjub_Base 'Auto I, KnownFFA Jubjub_Scalar 'Auto I) => Point -> Point -> FieldElement I -> Scalar
-hashToScalar _ _ m = fromUInt (from m)
+hashToScalar rPoint pubKey m = fromUInt (from (MiMC.hash (rPoint :*: pubKey :*: m)))
 
 specEdDSA :: Spec
 specEdDSA = describe "EdDSA verification (Jubjub, Poseidon Hash)" $ do
   it "verifies a correctly formed signature" $ do
     let g = pointGen @Point
         p = value @Jubjub_Scalar
-    forAll (fromConstant <$> toss p) $ \(x :: Scalar) ->
-      forAll (fromConstant <$> toss1 p) $ \(k :: Scalar) -> do
-        let a = x `scale` g
-            r = k `scale` g
-            m = zero :: FieldElement I
-            h = hashToScalar r a m
-            s = k + h * x
-            ok = eddsaVerify hashToScalar a m (r :*: s)
-            rAffine = SymAffine.affinePoint r
-        counterexample ("r = " <> show rAffine <> "\ns = " <> show s) $ evalBool ok === one
+    forAll (fromConstant <$> toss p) $ \(privKey :: Scalar) -> do
+      -- https://cryptobook.nakov.com/digital-signatures/eddsa-and-ed25519 for how to derive the signature.
+      let pubKey = privKey `scale` g
+          msg = zero :: FieldElement I
+          r = fromUInt (from (MiMC.hash (MiMC.hash privKey :*: msg)))
+          rPoint = r `scale` g
+          h = hashToScalar rPoint pubKey msg
+          s = r + h * privKey
+          ok = eddsaVerify hashToScalar pubKey msg (rPoint :*: s)
+          rAffine = SymAffine.affinePoint rPoint
+      counterexample ("r = " <> show rAffine <> "\ns = " <> show s) $ evalBool ok === one
 
   it "rejects tampered signatures and degenerate inputs" $ do
     let g = pointGen @Point
