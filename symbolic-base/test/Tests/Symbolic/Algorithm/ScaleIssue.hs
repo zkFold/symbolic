f5822c4c4fe24d19ec84400a1847a838287a877e
sourabhxyz <sourabhxyz@users.noreply.github.com>
2025-10-16 12:45:35 +0000

    fourmolu auto-commit

--- symbolic-base/test/Tests/Symbolic/Algorithm/ScaleIssue.hs
+++ symbolic-base/test/Tests/Symbolic/Algorithm/ScaleIssue.hs
@@ -5,6 +5,7 @@ module Tests.Symbolic.Algorithm.ScaleIssue (specScaleIssue) where
 
 import Data.Function (($))
 import GHC.Generics ((:*:) (..))
+import GHC.TypeNats (KnownNat)
 import Test.Hspec (Spec, it, shouldBe)
 import Text.Show (show)
 import Prelude (putStrLn, unlines, (<>))
@@ -12,24 +13,20 @@ import Prelude (putStrLn, unlines, (<>))
 import Tests.Common (evalBool)
 import ZkFold.Algebra.Class
 import ZkFold.Algebra.EllipticCurve.BLS12_381 (Fr)
-import ZkFold.Algebra.EllipticCurve.Jubjub (Jubjub_Base, Jubjub_Scalar)
 import ZkFold.Algebra.EllipticCurve.Class (pointGen)
 import ZkFold.Algebra.EllipticCurve.Jubjub (Jubjub_Base, Jubjub_Scalar)
+import ZkFold.Algebra.Number (value)
 import ZkFold.Data.Eq
 import qualified ZkFold.Symbolic.Algorithm.Hash.MiMC as MiMC
+import ZkFold.Symbolic.Class
+import ZkFold.Symbolic.Data.Combinators
 import ZkFold.Symbolic.Data.Combinators (RegisterSize (Auto), from)
 import ZkFold.Symbolic.Data.EllipticCurve.Jubjub (Jubjub_Point)
 import qualified ZkFold.Symbolic.Data.EllipticCurve.Point.Affine as SymAffine
 import ZkFold.Symbolic.Data.FFA (FFA, KnownFFA, fromUInt)
 import ZkFold.Symbolic.Data.FieldElement (FieldElement)
-import ZkFold.Symbolic.Interpreter (Interpreter)
-import qualified ZkFold.Symbolic.Algorithm.Hash.MiMC as MiMC
-import ZkFold.Data.Eq
-import ZkFold.Symbolic.Class
-import ZkFold.Symbolic.Data.Combinators
-import GHC.TypeNats (KnownNat)
 import ZkFold.Symbolic.Data.UInt (OrdWord, UInt)
-import ZkFold.Algebra.Number (value)
+import ZkFold.Symbolic.Interpreter (Interpreter)
 
 type I = Interpreter Fr
 
@@ -39,12 +36,13 @@ type Scalar = FFA Jubjub_Scalar 'Auto I
 
 scalarFieldFromFE
   :: forall p c n
-   . (Symbolic c, KnownFFA p 'Auto c
+   . ( Symbolic c
+     , KnownFFA p 'Auto c
      , n ~ NumberOfBits (BaseField c)
      , KnownRegisters c n 'Auto
      , KnownNat (GetRegisterSize (BaseField c) n 'Auto)
      , KnownNat (Ceil (GetRegisterSize (BaseField c) n 'Auto) OrdWord)
-   )
+     )
   => FieldElement c -> FFA p 'Auto c
 scalarFieldFromFE fe =
   let
@@ -53,7 +51,8 @@ scalarFieldFromFE fe =
    in
     fromUInt $ mod u m
 
-hashToScalar :: (KnownFFA Jubjub_Base 'Auto I, KnownFFA Jubjub_Scalar 'Auto I) => Point -> Point -> FieldElement I -> Scalar
+hashToScalar
+  :: (KnownFFA Jubjub_Base 'Auto I, KnownFFA Jubjub_Scalar 'Auto I) => Point -> Point -> FieldElement I -> Scalar
 hashToScalar rPoint pubKey m = scalarFieldFromFE (MiMC.hash (rPoint :*: pubKey :*: m))
 
 specScaleIssue :: Spec
@@ -67,7 +66,7 @@ specScaleIssue =
         rPoint = r `scale` g
         h = hashToScalar rPoint pubKey msg
         hpubKey' = (h * privKey) `scale` g
-        hpubKey = h `scale` pubKey  -- `hpubKey` and `hpubKey'` should both be equal.
+        hpubKey = h `scale` pubKey -- `hpubKey` and `hpubKey'` should both be equal.
         ok = SymAffine.affinePoint hpubKey == SymAffine.affinePoint hpubKey'
     putStrLn $
       unlines
