f10cbdf7ee5ae47e2ea4db89007e0eb504efef58
sourabhxyz <lambdaproofs@pm.me>
2025-10-16 18:46:17 +0530

    bug(#721): use FFAMaxBits for m

--- symbolic-base/test/Tests/Symbolic/Algorithm/ScaleIssue.hs
+++ symbolic-base/test/Tests/Symbolic/Algorithm/ScaleIssue.hs
@@ -5,7 +5,6 @@ module Tests.Symbolic.Algorithm.ScaleIssue (specScaleIssue) where
 
 import Data.Function (($))
 import GHC.Generics ((:*:) (..))
-import GHC.TypeNats (KnownNat)
 import Test.Hspec (Spec, it, shouldBe)
 import Text.Show (show)
 import Prelude (putStrLn, unlines, (<>))
@@ -20,12 +19,11 @@ import ZkFold.Data.Eq
 import qualified ZkFold.Symbolic.Algorithm.Hash.MiMC as MiMC
 import ZkFold.Symbolic.Class
 import ZkFold.Symbolic.Data.Combinators
-import ZkFold.Symbolic.Data.Combinators (RegisterSize (Auto), from)
 import ZkFold.Symbolic.Data.EllipticCurve.Jubjub (Jubjub_Point)
 import qualified ZkFold.Symbolic.Data.EllipticCurve.Point.Affine as SymAffine
-import ZkFold.Symbolic.Data.FFA (FFA, KnownFFA, fromUInt)
+import ZkFold.Symbolic.Data.FFA (FFA, KnownFFA, fromUInt, FFAMaxBits)
 import ZkFold.Symbolic.Data.FieldElement (FieldElement)
-import ZkFold.Symbolic.Data.UInt (OrdWord, UInt)
+import ZkFold.Symbolic.Data.UInt (UInt)
 import ZkFold.Symbolic.Interpreter (Interpreter)
 
 type I = Interpreter Fr
@@ -35,21 +33,18 @@ type Point = Jubjub_Point I
 type Scalar = FFA Jubjub_Scalar 'Auto I
 
 scalarFieldFromFE
-  :: forall p c n
+  :: forall p c
    . ( Symbolic c
      , KnownFFA p 'Auto c
-     , n ~ NumberOfBits (BaseField c)
-     , KnownRegisters c n 'Auto
-     , KnownNat (GetRegisterSize (BaseField c) n 'Auto)
-     , KnownNat (Ceil (GetRegisterSize (BaseField c) n 'Auto) OrdWord)
      )
   => FieldElement c -> FFA p 'Auto c
 scalarFieldFromFE fe =
   let
     u :: UInt (NumberOfBits (BaseField c)) 'Auto c = from fe
-    m = fromConstant (value @p)
+    uWide = resize u
+    m  :: UInt (FFAMaxBits p c) 'Auto c = fromConstant (value @p)
    in
-    fromUInt $ mod u m
+    fromUInt $ mod uWide m
 
 hashToScalar
   :: (KnownFFA Jubjub_Base 'Auto I, KnownFFA Jubjub_Scalar 'Auto I) => Point -> Point -> FieldElement I -> Scalar
