50969f374349b28deef8df9109f4ab4b60044557
sourabhxyz <lambdaproofs@pm.me>
2025-10-16 18:12:08 +0530

    bug(#721): add scalarFieldFromFE

--- symbolic-base/test/Tests/Symbolic/Algorithm/ScaleIssue.hs
+++ symbolic-base/test/Tests/Symbolic/Algorithm/ScaleIssue.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Tests.Symbolic.Algorithm.ScaleIssue (specScaleIssue) where
 
@@ -12,7 +13,6 @@ import Tests.Common (evalBool)
 import ZkFold.Algebra.Class
 import ZkFold.Algebra.EllipticCurve.BLS12_381 (Fr)
 import ZkFold.Algebra.EllipticCurve.Jubjub (Jubjub_Base, Jubjub_Scalar)
-import ZkFold.Symbolic.Data.Combinators (RegisterSize (Auto), from)
 import ZkFold.Algebra.EllipticCurve.Class (pointGen)
 import ZkFold.Symbolic.Data.EllipticCurve.Jubjub (Jubjub_Point)
 import qualified ZkFold.Symbolic.Data.EllipticCurve.Point.Affine as SymAffine
@@ -21,15 +21,34 @@ import ZkFold.Symbolic.Data.FieldElement (FieldElement)
 import ZkFold.Symbolic.Interpreter (Interpreter)
 import qualified ZkFold.Symbolic.Algorithm.Hash.MiMC as MiMC
 import ZkFold.Data.Eq
+import ZkFold.Symbolic.Class
+import ZkFold.Symbolic.Data.Combinators
+import GHC.TypeNats (KnownNat)
+import ZkFold.Symbolic.Data.UInt (OrdWord, UInt)
+import ZkFold.Algebra.Number (value)
 
 type I = Interpreter Fr
 type Point = Jubjub_Point I
 type Scalar = FFA Jubjub_Scalar 'Auto I
 
+scalarFieldFromFE
+  :: forall p c n
+   . (Symbolic c, KnownFFA p 'Auto c
+     , n ~ NumberOfBits (BaseField c)
+     , KnownRegisters c n 'Auto
+     , KnownNat (GetRegisterSize (BaseField c) n 'Auto)
+     , KnownNat (Ceil (GetRegisterSize (BaseField c) n 'Auto) OrdWord)
+   )
+  => FieldElement c -> FFA p 'Auto c
+scalarFieldFromFE fe =
+  let
+    u :: UInt (NumberOfBits (BaseField c)) 'Auto c = from fe
+    m = fromConstant (value @p)
+   in
+    fromUInt $ mod u m
+
 hashToScalar :: (KnownFFA Jubjub_Base 'Auto I, KnownFFA Jubjub_Scalar 'Auto I) => Point -> Point -> FieldElement I -> Scalar
-hashToScalar rPoint pubKey m =
-  let h0 = fromUInt (from (MiMC.hash (rPoint :*: pubKey :*: m)))
-  in h0 * one -- Trick to perform the modulo operation.
+hashToScalar rPoint pubKey m = scalarFieldFromFE (MiMC.hash (rPoint :*: pubKey :*: m))
 
 specScaleIssue :: Spec
 specScaleIssue = 
@@ -41,8 +60,7 @@ specScaleIssue =
         r :: Scalar = one + one  -- Works for `r = one`.
         rPoint = r `scale` g
         h = hashToScalar rPoint pubKey msg
-        hprivKey = h * privKey
-        hpubKey' = hprivKey `scale` g
+        hpubKey' = (h * privKey) `scale` g
         hpubKey = h `scale` pubKey  -- `hpubKey` and `hpubKey'` should both be equal.
         ok = SymAffine.affinePoint hpubKey == SymAffine.affinePoint hpubKey'
     putStrLn $ 
