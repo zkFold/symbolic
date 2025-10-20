9e26cfec682959f5b7c6647bd55daa883a486b4c
sourabhxyz <lambdaproofs@pm.me>
2025-10-20 18:36:15 +0530

    feat(#721): found issue with UInt <-> FFA conversion

--- symbolic-base/test/Tests/Symbolic/Algorithm/ScaleIssue.hs
+++ symbolic-base/test/Tests/Symbolic/Algorithm/ScaleIssue.hs
@@ -14,8 +14,12 @@ import ZkFold.Algebra.EllipticCurve.Jubjub (
 import ZkFold.Symbolic.Data.Combinators
 import ZkFold.Symbolic.Data.EllipticCurve.Jubjub (Jubjub_Point)
 import qualified ZkFold.Symbolic.Data.EllipticCurve.Point.Affine as SymAffine
-import ZkFold.Symbolic.Data.FFA (FFA)
+import ZkFold.Symbolic.Data.FFA (FFA, fromUInt)
 import ZkFold.Symbolic.Interpreter (Interpreter)
+import ZkFold.Symbolic.Data.FieldElement (FieldElement)
+import qualified ZkFold.Symbolic.Algorithm.Hash.MiMC as MiMC
+import ZkFold.Symbolic.Class (Symbolic(..))
+import ZkFold.Symbolic.Data.UInt (UInt)
 
 type I = Interpreter Fq
 
@@ -31,6 +35,18 @@ specScaleIssue =
         pubKey :: Point = privKey `scale` g
         orderNatural = order @Scalar
         orderFFA :: Scalar = fromConstant orderNatural
+        orderUInt = fromConstant orderNatural
+        -- orderFFAViaUInt :: Scalar = fromUInt (fromConstant orderNatural)
+        hashResult :: FieldElement I = MiMC.hash g
+        hashResultUInt :: UInt (NumberOfBits (BaseField I)) 'Auto I = from hashResult
+        hashResultUIntMod = hashResultUInt `mod` orderUInt
+        hashResultC1 = toConstant hashResult
+        hashResultC2 = toConstant hashResultUInt -- 'hashResultC1' is same as 'hashResultC2' and is also equal to the actual value computed manually, so UInt <-> FE conversation is likely correct.
+        hashResultMod = toConstant hashResultUIntMod
+        hashResultModFFA :: Scalar = fromUInt hashResultUIntMod
+        hashResultModFFAConstant = toConstant hashResultModFFA -- Should be same as 'hashResultMod'
+        -- hpubKey' = (hashResultModFFA * privKey) `scale` g
+        -- hpubKey = hashResultModFFA `scale` pubKey
     putStrLn $
       unlines
         [ "g = " <> show (SymAffine.affinePoint g)
@@ -38,8 +54,15 @@ specScaleIssue =
         , "pubKey = " <> show (SymAffine.affinePoint pubKey)
         , "orderNatural = " <> show orderNatural
         , "orderFFA = " <> show orderFFA
+        , "hashResult = " <> show hashResult
+        , "hashResultUInt = " <> show hashResultUInt
+        , "hashResultC1 = " <> show hashResultC1
+        , "hashResultC2 = " <> show hashResultC2
+        , "hashResultMod = " <> show hashResultMod
+        , "hashResultModFFAConstant = " <> show hashResultModFFAConstant -- Not same as 'hashResultMod'!
         ]
     SymAffine.affinePoint pubKey `shouldBe` SymAffine.affinePoint g
     SymAffine.affinePoint (((one :: Scalar) + one + one) `scale` g) `shouldBe` SymAffine.affinePoint (g + g + g)
     SymAffine.affinePoint (orderNatural `scale` g) `shouldBe` SymAffine.affinePoint (zero :: Point)
     SymAffine.affinePoint (orderFFA `scale` g) `shouldBe` SymAffine.affinePoint (zero :: Point)
+    -- SymAffine.affinePoint (hpubKey) `shouldBe` SymAffine.affinePoint (hpubKey')
