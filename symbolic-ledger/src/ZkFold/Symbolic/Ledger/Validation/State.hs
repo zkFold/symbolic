0f5d2f97fc58a71bbf934365a89c5503b8cc4943
sourabhxyz <lambdaproofs@pm.me>
2025-10-22 20:09:11 +0530

    feat(#724): use sum type to denote for error (temporarily)

--- symbolic-ledger/src/ZkFold/Symbolic/Ledger/Validation/State.hs
+++ symbolic-ledger/src/ZkFold/Symbolic/Ledger/Validation/State.hs
@@ -2,25 +2,33 @@
 
 module ZkFold.Symbolic.Ledger.Validation.State (
   validateStateUpdate,
+  validateStateUpdateEither,
   StateWitness (..),
+  StateUpdateValidationError (..),
+  Either' (..),
+  StateUpdateValidationResult,
 ) where
 
 import Data.Function ((&))
 import GHC.Generics ((:*:) (..), (:.:) (..))
+import GHC.Generics qualified as G
 import ZkFold.Algebra.Class (MultiplicativeMonoid (..), Zero (..), (+))
 import ZkFold.Control.Conditional (ifThenElse)
 import ZkFold.Data.Eq (Eq (..), (==))
 import ZkFold.Data.Vector (Vector, Zip (..))
 import ZkFold.Prelude (foldl')
-import ZkFold.Symbolic.Data.Bool (Bool, BoolType (..))
+import ZkFold.Symbolic.Data.Bool (Bool, BoolType (..), true)
 import ZkFold.Symbolic.Data.Hash (Hashable (..), hash, preimage)
 import ZkFold.Symbolic.Data.Hash qualified as Base
 import ZkFold.Symbolic.Data.MerkleTree (MerkleEntry)
 import ZkFold.Symbolic.Data.MerkleTree qualified as MerkleTree
+import ZkFold.Symbolic.Data.Sum (Sum, inject, match)
 
 import ZkFold.Symbolic.Ledger.Types
 import ZkFold.Symbolic.Ledger.Validation.Transaction (outputHasAtLeastOneAda)
 import ZkFold.Symbolic.Ledger.Validation.TransactionBatch (TransactionBatchWitness, validateTransactionBatch)
+import ZkFold.Data.HFunctor.Classes (HShow)
+import qualified Prelude as Haskell
 
 {- Note [State validation]
 
@@ -53,6 +61,28 @@ data StateWitness bi bo ud a i o t context = StateWitness
   , swTransactionBatch :: (TransactionBatchWitness ud i o a t) context
   }
 
+-- | Error cases for state update validation.
+data StateUpdateValidationError c
+  = PreviousStateHashMismatch
+  | LengthNotIncrementedByOne
+  | InvalidBridgeInWitness
+  | InvalidTransactionBatch
+  | UTxOTreeMismatch
+  deriving (G.Generic, G.Generic1)
+
+deriving stock instance (HShow context) => Haskell.Show (StateUpdateValidationError context)
+
+-- | A minimal Either-like sum for symbolic types.
+data Either' l r c
+  = Left' (Sum l c)
+  | Right' (r c)
+  deriving (G.Generic, G.Generic1)
+
+deriving stock instance (HShow context, Haskell.Show (r context), Haskell.Show (Sum l context)) => Haskell.Show (Either' l r context)
+
+-- | Concrete result type for state update validation with error information.
+type StateUpdateValidationResult = Sum (Either' StateUpdateValidationError Bool)
+
 -- | Validate state update. See note [State validation] for details.
 validateStateUpdate
   :: forall bi bo ud a i o t context
@@ -68,6 +98,26 @@ validateStateUpdate
   -- ^ Witness for the state.
   -> Bool context
 validateStateUpdate previousState action newState sw =
+  let res = validateStateUpdateEither previousState action newState sw
+   in match res (\e -> case e of
+        Left' _ -> false
+        Right' b -> b)
+
+-- | Validate state update and return either the first failing reason or success.
+validateStateUpdateEither
+  :: forall bi bo ud a i o t context
+   . SignatureState bi bo ud a context
+  => SignatureTransactionBatch ud i o a t context
+  => State bi bo ud a context
+  -- ^ Previous state.
+  -> TransactionBatch i o a t context
+  -- ^ The "action" that is applied to the state.
+  -> State bi bo ud a context
+  -- ^ New state.
+  -> StateWitness bi bo ud a i o t context
+  -- ^ Witness for the state.
+  -> StateUpdateValidationResult context
+validateStateUpdateEither previousState action newState sw =
   let
     initialUTxOTree = previousState.sUTxO
     bridgeInAssets = preimage newState.sBridgeIn
@@ -107,14 +157,22 @@ validateStateUpdate previousState action newState sw =
         bridgedInAssetsWithWitness
     bridgedOutOutputs = preimage newState.sBridgeOut
     (isBatchValid :*: utxoTree) = validateTransactionBatch utxoTreeWithBridgeIn bridgedOutOutputs action sw.swTransactionBatch
+    condHash = newState.sPreviousStateHash == hasher previousState
+    condLen  = newState.sLength == previousState.sLength + one
+    condBridgeIn = isWitBridgeInValid
+    condBatch = isBatchValid
+    condUTxO = utxoTree == newState.sUTxO
    in
-    -- New state correctly links to the previous state.
-    newState.sPreviousStateHash
-      == hasher previousState
-      && newState.sLength
-      == previousState.sLength
-      + one
-      && isWitBridgeInValid
-      && isBatchValid
-      && utxoTree
-      == newState.sUTxO
+    -- Return the first failing reason, or success flag.
+    ifThenElse condHash
+      ( ifThenElse condLen
+          ( ifThenElse condBridgeIn
+              ( ifThenElse condBatch
+                  ( ifThenElse condUTxO (inject (Right' true)) (inject (Left' (inject UTxOTreeMismatch))) )
+                  (inject (Left' (inject InvalidTransactionBatch)))
+              )
+              (inject (Left' (inject InvalidBridgeInWitness)))
+          )
+          (inject (Left' (inject LengthNotIncrementedByOne)))
+      )
+      (inject (Left' (inject PreviousStateHashMismatch)))
