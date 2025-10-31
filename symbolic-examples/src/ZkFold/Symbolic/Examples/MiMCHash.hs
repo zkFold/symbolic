651cfdfd85447d5b7d91c8a78ea236c8c831d1b7
TurtlePU <sokolov.p64@gmail.com>
2025-10-31 16:09:46 +0300

    fixed Examples except for SmartWallet

--- symbolic-examples/src/ZkFold/Symbolic/Examples/MiMCHash.hs
+++ symbolic-examples/src/ZkFold/Symbolic/Examples/MiMCHash.hs
@@ -2,8 +2,13 @@ module ZkFold.Symbolic.Examples.MiMCHash (exampleMiMC) where
 
 import ZkFold.Algebra.Class
 import ZkFold.Symbolic.Algorithm.Hash.MiMC (mimcConstants, mimcHash2)
-import ZkFold.Symbolic.Class (Symbolic (..))
 import ZkFold.Symbolic.Data.FieldElement (FieldElement)
+import ZkFold.Symbolic.V2 (Symbolic)
+import ZkFold.Symbolic.Compat (CompatData)
 
-exampleMiMC :: Symbolic c => FieldElement c -> FieldElement c -> FieldElement c
+exampleMiMC
+  :: Symbolic c
+  => CompatData FieldElement c
+  -> CompatData FieldElement c
+  -> CompatData FieldElement c
 exampleMiMC = mimcHash2 mimcConstants zero
--- symbolic-examples/src/ZkFold/Symbolic/Examples/UInt.hs
+++ symbolic-examples/src/ZkFold/Symbolic/Examples/UInt.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NoStarIsType #-}
 {-# LANGUAGE TypeOperators #-}
 
 module ZkFold.Symbolic.Examples.UInt (
@@ -15,7 +16,6 @@ import Data.Type.Equality (type (~))
 import GHC.Generics ((:*:) (..))
 import ZkFold.Algebra.Class
 import ZkFold.Algebra.Number (KnownNat, type (*))
-import ZkFold.Symbolic.Class (Symbolic (BaseField))
 import ZkFold.Symbolic.Data.Bool (Bool)
 import ZkFold.Symbolic.Data.Combinators (
   Ceil,
@@ -27,31 +27,43 @@ import ZkFold.Symbolic.Data.Combinators (
  )
 import ZkFold.Symbolic.Data.Ord ((<=))
 import ZkFold.Symbolic.Data.UInt (OrdWord, StrictNum (..), UInt, expMod, productMod)
+import ZkFold.Symbolic.V2 (Symbolic)
+import ZkFold.Symbolic.Compat (CompatData (CompatData, compatData))
+import Data.Function ((.))
 
 exampleUIntMul
   :: (KnownNat n, KnownRegisterSize r, Symbolic c)
-  => UInt n r c -> UInt n r c -> UInt n r c
+  => CompatData (UInt n r) c
+  -> CompatData (UInt n r) c
+  -> CompatData (UInt n r) c
 exampleUIntMul = (*)
 
 exampleUIntProductMod
   :: KnownNat n
   => KnownRegisterSize r
-  => KnownNat (Ceil (GetRegisterSize (BaseField c) n r) OrdWord)
-  => KnownNat (NumberOfRegisters (BaseField c) n r)
+  => KnownNat (Ceil (GetRegisterSize c n r) OrdWord)
+  => KnownRegisters c n r
   => Symbolic c
-  => UInt n r c -> UInt n r c -> UInt n r c -> (UInt n r :*: UInt n r) c
-exampleUIntProductMod x y z = let (p, m) = productMod x y z in p :*: m
+  => CompatData (UInt n r) c
+  -> CompatData (UInt n r) c
+  -> CompatData (UInt n r) c
+  -> (CompatData (UInt n r) :*: CompatData (UInt n r)) c
+exampleUIntProductMod (CompatData x) (CompatData y) (CompatData z) =
+  let (p, m) = productMod x y z in CompatData p :*: CompatData m
 
 exampleUIntDivMod
   :: ( KnownNat n
      , KnownRegisterSize r
      , Symbolic c
-     , NumberOfRegisters (BaseField c) n r ~ k
+     , NumberOfRegisters c n r ~ k
      , KnownNat k
-     , KnownNat (Ceil (GetRegisterSize (BaseField c) n r) OrdWord)
+     , KnownNat (Ceil (GetRegisterSize c n r) OrdWord)
      )
-  => UInt n r c -> UInt n r c -> (UInt n r :*: UInt n r) c
-exampleUIntDivMod x y = let (d, m) = divMod x y in d :*: m
+  => CompatData (UInt n r) c
+  -> CompatData (UInt n r) c
+  -> (CompatData (UInt n r) :*: CompatData (UInt n r)) c
+exampleUIntDivMod (CompatData x) (CompatData y) =
+  let (d, m) = divMod x y in CompatData d :*: CompatData m
 
 exampleUIntExpMod
   :: forall n p m r c
@@ -61,36 +73,44 @@ exampleUIntExpMod
   => KnownNat n
   => KnownNat m
   => KnownNat (2 * m)
-  => KnownRegisters (BaseField c) (2 * m) r
-  => KnownNat (Ceil (GetRegisterSize (BaseField c) (2 * m) r) OrdWord)
-  => UInt n r c
-  -> UInt p r c
-  -> UInt m r c
-  -> UInt m r c
-exampleUIntExpMod = expMod @c @n @p @m @r
+  => KnownRegisters c (2 * m) r
+  => KnownNat (Ceil (GetRegisterSize c (2 * m) r) OrdWord)
+  => CompatData (UInt n r) c
+  -> CompatData (UInt p r) c
+  -> CompatData (UInt m r) c
+  -> CompatData (UInt m r) c
+exampleUIntExpMod (CompatData x) (CompatData y) (CompatData m) =
+  CompatData (expMod @_ @n @p @m @r x y m)
 
 exampleUIntStrictAdd
   :: (KnownNat n, KnownRegisterSize r, Symbolic c)
-  => UInt n r c -> UInt n r c -> UInt n r c
-exampleUIntStrictAdd = strictAdd
+  => CompatData (UInt n r) c
+  -> CompatData (UInt n r) c
+  -> CompatData (UInt n r) c
+exampleUIntStrictAdd (CompatData x) (CompatData y) = CompatData (strictAdd x y)
 
 exampleUIntStrictMul
   :: (KnownNat n, KnownRegisterSize r, Symbolic c)
-  => UInt n r c -> UInt n r c -> UInt n r c
-exampleUIntStrictMul = strictMul
+  => CompatData (UInt n r) c
+  -> CompatData (UInt n r) c
+  -> CompatData (UInt n r) c
+exampleUIntStrictMul (CompatData x) (CompatData y) = CompatData (strictMul x y)
 
 exampleUIntResize
   :: (KnownNat n, KnownNat k, KnownRegisterSize r, Symbolic c)
-  => UInt n r c -> UInt k r c
-exampleUIntResize = resize
+  => CompatData (UInt n r) c
+  -> CompatData (UInt k r) c
+exampleUIntResize = CompatData . resize . compatData
 
 exampleUIntLeq
   :: ( KnownNat n
      , KnownRegisterSize r
      , Symbolic c
-     , KnownRegisters (BaseField c) n r
-     , regSize ~ GetRegisterSize (BaseField c) n r
+     , KnownRegisters c n r
+     , regSize ~ GetRegisterSize c n r
      , KnownNat (Ceil regSize OrdWord)
      )
-  => UInt n r c -> UInt n r c -> Bool c
+  => CompatData (UInt n r) c
+  -> CompatData (UInt n r) c
+  -> CompatData Bool c
 exampleUIntLeq = (<=)
