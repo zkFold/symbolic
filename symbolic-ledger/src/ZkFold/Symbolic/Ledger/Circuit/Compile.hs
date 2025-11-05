7515570d7434bc67fbd7589ae6bddb74e9631919
sourabhxyz <sourabhxyz@users.noreply.github.com>
2025-11-05 15:56:36 +0000

    fourmolu auto-commit

--- symbolic-ledger/src/ZkFold/Symbolic/Ledger/Circuit/Compile.hs
+++ symbolic-ledger/src/ZkFold/Symbolic/Ledger/Circuit/Compile.hs
@@ -7,21 +7,14 @@ module ZkFold.Symbolic.Ledger.Circuit.Compile (
 
 -- TODO: Refine import from SmartWallet.
 
-import GHC.TypeNats (type (^), type (+))
 import Data.Type.Equality (type (~))
 import GHC.Generics (Generic, Generic1, Par1, U1, (:*:))
-import ZkFold.Algebra.Polynomial.Univariate (PolyVec)
-import ZkFold.Protocol.Plonkup.Proof
-import qualified ZkFold.Algebra.Number as Number
-import ZkFold.Protocol.Plonkup.Prover.Secret (PlonkupProverSecret (..))
-import ZkFold.Protocol.Plonkup.Relation (PlonkupRelation (..))
-import ZkFold.Protocol.Plonkup.Utils (getParams)
-import ZkFold.Protocol.Plonkup.Verifier.Commitments
-import ZkFold.Protocol.Plonkup.Verifier.Setup
-import ZkFold.Protocol.Plonkup.Witness (PlonkupWitnessInput (..))
+import GHC.TypeNats (type (+), type (^))
 import ZkFold.Algebra.Class
-import ZkFold.Protocol.Plonkup (Plonkup (..))
+import ZkFold.Algebra.EllipticCurve.BLS12_381 (BLS12_381_G1_JacobianPoint, BLS12_381_G2_JacobianPoint)
 import ZkFold.Algebra.EllipticCurve.Jubjub (Fq)
+import ZkFold.Algebra.Number qualified as Number
+import ZkFold.Algebra.Polynomial.Univariate (PolyVec)
 import ZkFold.ArithmeticCircuit
 import ZkFold.Protocol.NonInteractiveProof as NP (
   FromTranscript (..),
@@ -30,18 +23,25 @@ import ZkFold.Protocol.NonInteractiveProof as NP (
   TrustedSetup (..),
   powersOfTauSubset,
  )
-import ZkFold.Symbolic.Examples.SmartWallet hiding (PlonkupTs)
+import ZkFold.Protocol.Plonkup (Plonkup (..))
+import ZkFold.Protocol.Plonkup.Proof
+import ZkFold.Protocol.Plonkup.Prover.Secret (PlonkupProverSecret (..))
+import ZkFold.Protocol.Plonkup.Relation (PlonkupRelation (..))
+import ZkFold.Protocol.Plonkup.Utils (getParams)
+import ZkFold.Protocol.Plonkup.Verifier.Commitments
+import ZkFold.Protocol.Plonkup.Verifier.Setup
+import ZkFold.Protocol.Plonkup.Witness (PlonkupWitnessInput (..))
 import ZkFold.Symbolic.Class (BaseField)
 import ZkFold.Symbolic.Compiler qualified as C
 import ZkFold.Symbolic.Data.Bool
 import ZkFold.Symbolic.Data.Class
 import ZkFold.Symbolic.Data.Input (SymbolicInput)
 import ZkFold.Symbolic.Data.Vec (Vec (..), runVec)
+import ZkFold.Symbolic.Examples.SmartWallet hiding (PlonkupTs)
 import Prelude (($))
 
 import ZkFold.Symbolic.Ledger.Types
 import ZkFold.Symbolic.Ledger.Validation.State
-import ZkFold.Algebra.EllipticCurve.BLS12_381 (BLS12_381_G1_JacobianPoint, BLS12_381_G2_JacobianPoint)
 
 data LedgerContractInput bi bo ud a i o t c = LedgerContractInput
   { lciPreviousState :: State bi bo ud a c
@@ -93,7 +93,7 @@ type PlonkupTs i n t = Plonkup i Par1 n BLS12_381_G1_JacobianPoint BLS12_381_G2_
 ledgerSetup
   :: forall tc bi bo ud a i o t c
    . TranscriptConstraints tc
-  =>  SignatureState bi bo ud a c
+  => SignatureState bi bo ud a c
   => SignatureTransactionBatch ud i o a t c
   => TrustedSetup (LedgerCircuitGates + 6)
   -> LedgerCircuit bi bo ud a i o t
