e3d6f8f43e73a75169fbbb2ae50e1d70bff626c9
sourabhxyz <lambdaproofs@pm.me>
2025-11-07 19:45:52 +0530

    feat(#742): add ToJSON instances

--- symbolic-ledger/src/ZkFold/Symbolic/Ledger/Types/Orphans.hs
+++ symbolic-ledger/src/ZkFold/Symbolic/Ledger/Types/Orphans.hs
@@ -11,7 +11,7 @@ import Control.Applicative (pure)
 import Data.Function (($))
 import Data.Functor ((<$>))
 import Data.Kind (Type)
-import GHC.Generics (Generic, Generic1, (:.:) (..))
+import GHC.Generics (Generic, Generic1, Par1 (..), (:.:) (..))
 import ZkFold.Data.Vector (Vector)
 import ZkFold.Symbolic.Class (Ctx, Symbolic)
 import ZkFold.Symbolic.Data.Class (SymbolicData)
@@ -22,11 +22,13 @@ import ZkFold.Symbolic.Data.Hash qualified as Base
 import ZkFold.Symbolic.Ledger.Types.Hash
 import ZkFold.Algebra.EllipticCurve.Jubjub (Fq)
 import ZkFold.Symbolic.Data.MerkleTree (MerkleEntry)
-import Data.Aeson (FromJSON (..), withBool)
-import ZkFold.Symbolic.Interpreter (Interpreter)
-import ZkFold.Algebra.Class (FromConstant (..))
+import Data.Aeson (FromJSON (..), ToJSON (..), withBool)
+import ZkFold.Symbolic.Interpreter (Interpreter (..))
+import ZkFold.Algebra.Class (FromConstant (..), MultiplicativeMonoid (..), ToConstant (..))
 import qualified ZkFold.Symbolic.Data.Bool as SBool
 import Prelude (Integer)
+import qualified Prelude as Haskell
+import ZkFold.Symbolic.Data.Bool (fromBool)
 
 newtype VectorTakingCtx n (a :: Ctx -> Type) c = VectorTakingCtx ((Vector n :.: a) c)
   deriving stock (Generic, Generic1)
@@ -49,7 +51,18 @@ instance FromJSON (FieldElement (Interpreter Fq)) where
 instance FromJSON (SBool.Bool (Interpreter Fq)) where
   parseJSON = withBool "Bool" $ \b -> pure $ fromConstant b
 
+instance ToJSON (FieldElement (Interpreter Fq)) where
+  toJSON v = toJSON (toConstant v :: Fq)
+
+
+instance ToJSON (SBool.Bool (Interpreter Fq)) where
+  toJSON b = toJSON (fromBool b Haskell.== one)
+
 instance forall n. FromJSON (SBool.Bool (Interpreter Fq)) => FromJSON ((:.:) (Vector n) SBool.Bool (Interpreter Fq)) where
   parseJSON v = Comp1 <$> parseJSON v
 
+instance forall n. ToJSON (SBool.Bool (Interpreter Fq)) => ToJSON ((:.:) (Vector n) SBool.Bool (Interpreter Fq)) where
+  toJSON (Comp1 x) = toJSON x
+
 deriving anyclass instance forall ud. FromJSON (MerkleEntry ud (Interpreter Fq))
+deriving anyclass instance forall ud. ToJSON   (MerkleEntry ud (Interpreter Fq))
