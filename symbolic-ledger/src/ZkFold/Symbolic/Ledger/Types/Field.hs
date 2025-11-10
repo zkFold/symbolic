17e16269942506806c14035bc9f550b3ea679c72
sourabhxyz <lambdaproofs@pm.me>
2025-11-10 19:48:46 +0530

    docs(#742): document Field module

--- symbolic-ledger/src/ZkFold/Symbolic/Ledger/Types/Field.hs
+++ symbolic-ledger/src/ZkFold/Symbolic/Ledger/Types/Field.hs
@@ -6,6 +6,8 @@ module ZkFold.Symbolic.Ledger.Types.Field (
 import ZkFold.Algebra.EllipticCurve.Jubjub (Fq)
 import ZkFold.Symbolic.Interpreter (Interpreter)
 
+-- | Base field used for the rollup ledger.
 type RollupBF = Fq
 
+-- | Interpreter for the rollup base field.
 type RollupBFInterpreter = Interpreter RollupBF
