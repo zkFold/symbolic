344b6bce3fec7428dc58febc5a49a623de742119
sourabhxyz <lambdaproofs@pm.me>
2025-12-02 20:20:12 +0530

    feat(#772): update bridge out output to represent an actual cardano address

--- symbolic-ledger/src/ZkFold/Symbolic/Ledger/Examples/One.hs
+++ symbolic-ledger/src/ZkFold/Symbolic/Ledger/Examples/One.hs
@@ -89,6 +89,9 @@ adaAsset =
 
 bridgeInOutput = Output {oAddress = address, oAssets = adaAsset}
 
+-- Cardano address @addr_test1qpxsldf6hmp5vtdhhwzukm8x5q0m9t2xh8cftx8s6a43vll3t8hyc5syfx9lltq9dgr2xdkvwahr9humhpa9tae2jcjsxpxw2h@ (picked randomly from explorer), maps to @4d0fb53abec3462db7bb85cb6ce6a01fb2ad46b9f09598f0d76b167f@ payment credential and @f159ee4c5204498bffac056a06a336cc776e32df9bb87a55f72a9625@ staking credential. And this concatenated hex string (where we concatenate payment credential and staking credential) maps to following base field given the way we convert hex strings to base fields in our rollup plutus validator.
+bridgeOutOutput = Output {oAddress = fromConstant (49774046922339375371572345018363912853151769526542625924134849613540002678757 :: Natural), oAssets = adaAsset}
+
 -- We bridge in an output and refer to it in transaction.
 bridgedIn :: (Vector Bi :.: Output A) I
 bridgedIn = Comp1 (fromList [bridgeInOutput])
@@ -117,7 +120,7 @@ tx2 :: Transaction Ixs Oxs A I
 tx2 =
   Transaction
     { inputs = Comp1 (fromList [OutputRef {orTxId = txId tx & Base.hHash, orIndex = zero}])
-    , outputs = Comp1 (fromList [bridgeInOutput :*: true])
+    , outputs = Comp1 (fromList [bridgeOutOutput :*: true])
     }
 
 bridgedIn2 :: (Vector Bi :.: Output A) I
