c3a9e9a7ecb34956a2dca2028d5e942acaeaa972
ajuggler <ajuggler@users.noreply.github.com>
2026-01-16 18:04:48 +0000

    fourmolu auto-commit

--- symbolic-base/src/ZkFold/Symbolic/Algorithm/UTxO/Membership.hs
+++ symbolic-base/src/ZkFold/Symbolic/Algorithm/UTxO/Membership.hs
@@ -8,7 +8,6 @@ import GHC.TypeLits (KnownNat, Nat)
 import ZkFold.Algebra.Class hiding (Euclidean (..))
 import ZkFold.Control.Conditional (ifThenElse)
 import ZkFold.Data.Vector (Vector)
-
 import ZkFold.Symbolic.Algorithm.Mithril (StakeDistribution, mithril)
 import ZkFold.Symbolic.Class (BaseField, Symbolic)
 import ZkFold.Symbolic.Data.Combinators (GetRegisterSize, NumberOfRegisters, RegisterSize (Auto))
@@ -19,30 +18,29 @@ import ZkFold.Symbolic.Data.FieldElement (FieldElement)
 data Direction = LeftDir | RightDir
 
 data MerklePath (d :: Nat) ctx = MerklePath
-  { mpSiblings   :: Vector d (FieldElement ctx)
+  { mpSiblings :: Vector d (FieldElement ctx)
   , mpDirections :: Vector d (FieldElement ctx)
   }
 
-newtype PaddedBytes (len :: Nat) ctx = PaddedBytes { unPaddedBytes :: Vector len (FieldElement ctx) }
+newtype PaddedBytes (len :: Nat) ctx = PaddedBytes {unPaddedBytes :: Vector len (FieldElement ctx)}
 
 -- | Public parameters.
 data UTxOParams m d outLen point p q ctx = UTxOParams
-  { upStakeDist   :: StakeDistribution m point ctx
-  , upThreshold   :: FieldElement ctx
+  { upStakeDist :: StakeDistribution m point ctx
+  , upThreshold :: FieldElement ctx
   , upSignatureRS :: (FFA p 'Auto ctx, FFA q 'Auto ctx)
-  , upUtxoRoot    :: FieldElement ctx
+  , upUtxoRoot :: FieldElement ctx
   }
 
 -- | Witness (private inputs).
 data UTxOWitness d outLen ctx = UTxOWitness
   { uwLeafKeyHash :: FieldElement ctx
   , uwTxOutPadded :: PaddedBytes outLen ctx
-  , uwMerklePath  :: MerklePath d ctx
+  , uwMerklePath :: MerklePath d ctx
   }
 
 -- | Circuit output.
 data UtxOOutput outLen ctx = UtxOOutput
-  { uoOk             :: FieldElement ctx
+  { uoOk :: FieldElement ctx
   , uoTxOutPaddedOut :: PaddedBytes outLen ctx
   }
-
