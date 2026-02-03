de058e81fe37b54485efd6641bc2ebe50fe21e42
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2026-02-03 16:39:57 +0100

    Add circuit sizes for symbolic ledger

--- symbolic-ledger/bench/CircuitSize.hs
+++ symbolic-ledger/bench/CircuitSize.hs
@@ -1,20 +1,20 @@
 module Main where
 
-import Control.Applicative (Applicative (..))
+import Control.Applicative (pure)
 import Data.ByteString.Lazy (ByteString)
 import Data.Function (($))
 import Data.Semigroup (Semigroup (..))
 import Data.String (String)
 import Data.String qualified as String
 import System.IO (IO)
+import Test.Tasty (TestTree)
 import Test.Tasty qualified as Tasty
 import Test.Tasty.Golden qualified as Golden
 import Text.Show (Show (..))
 import ZkFold.ArithmeticCircuit (ArithmeticCircuit)
 import ZkFold.ArithmeticCircuit qualified as Circuit
 
-import ZkFold.Symbolic.Ledger.Circuit.Compile (LedgerCircuit, ledgerCircuit)
-import ZkFold.Symbolic.Ledger.Examples.One (A, Bi, Bo, Ixs, Oxs, TxCount, Ud)
+import ZkFold.Symbolic.Ledger.Circuit.Compile (ledgerCircuit)
 import ZkFold.Symbolic.Ledger.Types.Field (RollupBFInterpreter)
 
 metrics :: String -> ArithmeticCircuit a i o -> ByteString
@@ -29,15 +29,24 @@ metrics name circuit =
     <> "\nNumber of lookup tables: "
     <> String.fromString (show $ Circuit.acSizeT circuit)
 
+goldenCircuit :: String -> ArithmeticCircuit a i o -> TestTree
+goldenCircuit name circuit =
+  Golden.goldenVsString name ("stats/" <> name) $ pure (metrics name circuit)
+
+-- | Ledger circuit type parameters:
+-- @bi@      - number of bridge-in outputs
+-- @bo@      - number of bridge-out outputs
+-- @ud@      - UTXO tree depth (2^(ud-1) leaves)
+-- @a@       - number of assets per output
+-- @ixs@     - number of inputs per transaction
+-- @oxs@     - number of outputs per transaction
+-- @txCount@ - number of transactions per batch
 main :: IO ()
 main =
   Tasty.defaultMain $
     Tasty.testGroup
       "Ledger circuit golden tests"
-      [ Golden.goldenVsString name ("stats/" <> name) $ pure (metrics name circuit)
-      | (name, circuit) <- ledgerExamples
+      [ goldenCircuit "Ledger.1.1.2.1.1.1.1" (ledgerCircuit @1 @1 @2 @1 @1 @1 @1 @RollupBFInterpreter)
+      , goldenCircuit "Ledger.1.1.2.1.1.1.2" (ledgerCircuit @1 @1 @2 @1 @1 @1 @2 @RollupBFInterpreter)
+      , goldenCircuit "Ledger.1.1.4.1.1.1.2" (ledgerCircuit @1 @1 @4 @1 @1 @1 @2 @RollupBFInterpreter)
       ]
-
-ledgerExamples :: [(String, LedgerCircuit Bi Bo Ud A Ixs Oxs TxCount)]
-ledgerExamples =
-  [("Ledger.1.1.2.1.1.1.1", ledgerCircuit @Bi @Bo @Ud @A @Ixs @Oxs @TxCount @RollupBFInterpreter)]
