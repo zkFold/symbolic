4a81ed76ff057d3bbe9aad54e62b16da7cc4935d
sourabhxyz <lambdaproofs@pm.me>
2026-02-20 18:14:03 +0530

    feat: try IVC in ledger

--- symbolic-ledger/test/Main.hs
+++ symbolic-ledger/test/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Test.Hspec (hspec)
 import Prelude (IO, ($))
 
+import Tests.Symbolic.Ledger.E2E.IVC (specIVC)
 import Tests.Symbolic.Ledger.JSON.LedgerContractInputGolden (specLedgerContractInputJSON)
 import Tests.Symbolic.Ledger.Update (specUpdateLedgerState)
 
@@ -10,3 +11,4 @@ main :: IO ()
 main = hspec $ do
   specUpdateLedgerState
   specLedgerContractInputJSON
+  specIVC
