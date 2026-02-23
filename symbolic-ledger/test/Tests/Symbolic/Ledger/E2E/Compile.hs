0f89455852e8b996fd57c0fabf6f6e547ba12bb3
sourabhxyz <lambdaproofs@pm.me>
2026-02-23 18:56:19 +0530

    feat: undo uncomment

--- symbolic-ledger/test/Tests/Symbolic/Ledger/E2E/Compile.hs
+++ symbolic-ledger/test/Tests/Symbolic/Ledger/E2E/Compile.hs
@@ -4,10 +4,10 @@ import Test.Hspec (Spec)
 
 import Tests.Symbolic.Ledger.E2E.Compile.One
 import Tests.Symbolic.Ledger.E2E.Compile.Three
-import Tests.Symbolic.Ledger.E2E.Compile.Two
+-- import Tests.Symbolic.Ledger.E2E.Compile.Two
 
 specE2ECompile :: Spec
 specE2ECompile = do
   specE2ECompileOne
-  specE2ECompileTwo
+  -- specE2ECompileTwo
   specE2ECompileThree
