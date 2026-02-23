d87c7f6cdbed875e0dfad82b53973e82606d2eea
sourabhxyz <sourabhxyz@users.noreply.github.com>
2026-02-23 13:27:02 +0000

    fourmolu auto-commit

--- symbolic-ledger/test/Tests/Symbolic/Ledger/E2E/Compile.hs
+++ symbolic-ledger/test/Tests/Symbolic/Ledger/E2E/Compile.hs
@@ -4,6 +4,7 @@ import Test.Hspec (Spec)
 
 import Tests.Symbolic.Ledger.E2E.Compile.One
 import Tests.Symbolic.Ledger.E2E.Compile.Three
+
 -- import Tests.Symbolic.Ledger.E2E.Compile.Two
 
 specE2ECompile :: Spec
