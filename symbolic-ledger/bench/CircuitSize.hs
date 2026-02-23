8045827f00f93babe773ab780bd4dca959fce151
sourabhxyz <lambdaproofs@pm.me>
2026-02-23 18:05:23 +0530

    feat: revert changes in bench

--- symbolic-ledger/bench/CircuitSize.hs
+++ symbolic-ledger/bench/CircuitSize.hs
@@ -49,6 +49,4 @@ main =
       [ goldenCircuit "Ledger.1.1.2.1.1.1.1" (ledgerCircuit @1 @1 @2 @1 @1 @1 @1 @RollupBFInterpreter)
       , goldenCircuit "Ledger.1.1.2.1.1.1.2" (ledgerCircuit @1 @1 @2 @1 @1 @1 @2 @RollupBFInterpreter)
       , goldenCircuit "Ledger.1.1.4.1.1.1.2" (ledgerCircuit @1 @1 @4 @1 @1 @1 @2 @RollupBFInterpreter)
-      , goldenCircuit "Ledger.1.1.4.2.2.2.2" (ledgerCircuit @1 @1 @4 @2 @2 @2 @2 @RollupBFInterpreter)
-      , goldenCircuit "Ledger.1.1.3.2.2.2.3" (ledgerCircuit @1 @1 @3 @2 @2 @2 @3 @RollupBFInterpreter)
       ]
