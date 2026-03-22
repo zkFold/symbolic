a96f57fb9ea7077cbd98597242606963039536cf
sourabhxyz <lambdaproofs@pm.me>
2026-03-22 16:34:47 +0530

    feat(#808): add setup generator

--- symbolic-base/test/SetupGenerator.hs
+++ symbolic-base/test/SetupGenerator.hs
@@ -1,35 +1,37 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeOperators #-}
 
 module Main where
 
-import GHC.TypeNats (type (+), type (^))
+import GHC.TypeNats (SomeNat (..), someNatVal)
+import Data.Proxy (Proxy (..))
 import System.Environment (getArgs)
 import Prelude
 
-import ZkFold.Protocol.NonInteractiveProof.TrustedSetup (TrustedSetup (..), readTrustedSetup, saveTrustedSetup)
-
--- We need 2^20 + 6 points.
-type N = 2 ^ 20 + 6
+import ZkFold.Algebra.Number (value)
+import ZkFold.Protocol.NonInteractiveProof.TrustedSetup (readTrustedSetup, saveTrustedSetup)
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [inputPath] -> extractSetup inputPath "data/midnight_powers_of_tau_2e20"
-    [inputPath, outputPath] -> extractSetup inputPath outputPath
-    _ -> putStrLn "Usage: setup-generator <input-midnight-file> [output-file]"
+    [nStr, inputPath, outputPath] ->
+      case reads nStr of
+        [(n, "")] | n > 0 -> extractSetup n inputPath outputPath
+        _ -> putStrLn "ERROR: first argument must be a positive integer (number of G1 points)"
+    _ -> putStrLn "Usage: setup-generator <n-points> <input-midnight-file> <output-file>"
 
-extractSetup :: FilePath -> FilePath -> IO ()
-extractSetup inputPath outputPath = do
-  putStrLn $ "Extracting 2^20 + 6 G1 points from: " ++ inputPath
-  -- The original Midnight file has uncompressed G1 points (96 bytes each)
-  result <- readTrustedSetup @N inputPath False
-  case result of
-    Nothing -> putStrLn "ERROR: Failed to read trusted setup (points not on curve?)"
-    Just ts -> do
-      putStrLn $ "Successfully read " ++ show (2 ^ (20 :: Int) + 6) ++ " G1 points and 2 G2 points"
-      putStrLn $ "Saving compressed to: " ++ outputPath
-      saveTrustedSetup outputPath True ts
-      putStrLn "Done."
+extractSetup :: Integer -> FilePath -> FilePath -> IO ()
+extractSetup n inputPath outputPath =
+  case someNatVal (fromIntegral n) of
+    SomeNat (_ :: Proxy n) -> do
+      putStrLn $ "Extracting " ++ show n ++ " G1 points from: " ++ inputPath
+      -- The original Midnight file has uncompressed G1 points (96 bytes each)
+      result <- readTrustedSetup @n inputPath False
+      case result of
+        Nothing -> putStrLn "ERROR: Failed to read trusted setup (points not on curve?)"
+        Just ts -> do
+          putStrLn $ "Successfully read " ++ show (value @n) ++ " G1 points and 2 G2 points"
+          putStrLn $ "Saving compressed to: " ++ outputPath
+          saveTrustedSetup outputPath True ts
+          putStrLn "Done."
